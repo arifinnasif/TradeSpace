151d75941f1e0e17e422e14dc13e5d285c46b768
Sultanul Arifin <msa336@outlook.com>
2023-07-06 02:06:40 +0600

    demo RESTful api done with ORM

--- backend/src/routers/things/things.ts
+++ backend/src/routers/things/things.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { get_a_thing, create_a_thing, update_a_thing, delete_a_thing } from "../../controllers/things.controller";
+import { get_all_things, get_a_thing, create_a_thing, update_a_thing, delete_a_thing } from "../../controllers/things.controller";
 
 let router = express.Router();
 
@@ -8,11 +8,15 @@ router.use((req, res, next) => {
     next();
 });
 
+router.route("/")
+
+    .get(get_all_things)
+
+    .post(create_a_thing);
+
 router.route("/:thingId")
 
     .get(get_a_thing)
-
-    .post(create_a_thing)
 
     .put(update_a_thing)
 

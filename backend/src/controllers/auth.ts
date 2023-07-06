b44f233baa9a903160121b47e1652b233efdccbb
ansakib <1705107@ugrad.cse.buet.ac.bd>
2023-07-06 13:08:53 +0600

    prisma problem

--- backend/src/controllers/auth.ts
+++ backend/src/controllers/auth.ts
@@ -1,10 +1,14 @@
-const db = require('../db') 
 import { Request, Response} from 'express';
+
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 export let getUsers = async (req:Request, res:Response) => {
     try {
-        const userList = await db.query("SELECT * FROM users")
-        console.log(userList)
+        const userList = await prisma.users.findMany();
+        console.log(userList);
+        res.json(userList);
     } catch (error) {
         console.log(error)
     }

65c523b64fd458513416f8bde572756973605a93
Sheam1685 <samia12676@gmail.com>
2023-07-06 23:44:16 +0600

    frontend modularized further

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,29 @@
 import './App.css';
 import Signup from './components/Signup';
-import { Container } from '@mui/material';
+import Container from '@mui/material/Container';
+import CssBaseline from '@mui/material/CssBaseline';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+// const defaultTheme = createTheme();
+const defaultTheme = createTheme({
+  palette: {
+    primary: {
+      main: '#00bfa5',
+    },
+    secondary: {
+      main: '#00bfa5',
+    },
+  },
+});
 
 function App() {
   return (
-    <Container>
-      <Signup />
-    </Container>
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="xs">
+      <CssBaseline />
+        <Signup />
+      </Container>
+    </ThemeProvider>
   );
 }
 

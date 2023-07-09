24dec1d537b49e0800106948925d3636d6c0b727
ansakib <1705107@ugrad.cse.buet.ac.bd>
2023-07-09 13:38:13 +0600

    query string template

--- backend/src/controllers/things.controller.ts
+++ backend/src/controllers/things.controller.ts
@@ -1,12 +1,57 @@
+// @ts-nocheck
 import { Request, Response, NextFunction } from 'express';
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
 export let get_all_things = async (req: Request, res: Response) => {
-    console.log("accessing all things");
-    const all_things = await prisma.stuffs.findMany();
-    res.json(all_things);
+    // console.log("accessing all things");
+    // const all_things = await prisma.stuffs.findMany();
+    // res.json(all_things);
+    const apples = [
+        {
+            type: 'Empire',
+            price: '$1.20',
+            quantityAvailable: 10,
+        },
+        {
+            type: 'Granny Smith',
+            price: '$0.85',
+            quantityAvailable: 8,
+        },
+        {
+            type: 'Gala',
+            price: '$0.98',
+            quantityAvailable: 4,
+        },
+        {
+            type: 'Fuji',
+            price: '$0.95',
+            quantityAvailable: 3,
+        },
+        {
+            type: 'Honeycrisp',
+            price: '$1.50',
+            quantityAvailable: 3,
+        },
+        {
+            type: 'Golden Delicious',
+            price: '$1.20',
+            quantityAvailable: 2,
+        },
+        {
+            type: 'Red Delicious',
+            price: '$1.10',
+            quantityAvailable: 11,
+        },
+    ];
+    const { filterBy = [] } = req.query;
+    console.log(Array.isArray(filterBy)); // true
+
+    const filteredApples = apples.filter((apple) =>
+        filterBy.includes(apple.type)
+    );
+    res.json(filteredApples);
 }
 
 export let get_a_thing = async (req: Request, res: Response) => {
--- backend/src/routers/index.ts
+++ backend/src/routers/index.ts
@@ -4,9 +4,11 @@ const router = express.Router();
 
 //import routers
 import authRouter from "./auth/auth";
+import thingsRouter from "./things/things"
 
 
 // initialize routers
 router.use("/auth", authRouter);
+router.use("/things", thingsRouter);
 
 export default router;

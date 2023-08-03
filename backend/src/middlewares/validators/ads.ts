bb1189bd29b4a34366856c81cf9f16d159dc196a
Sheam1685 <samia12676@gmail.com>
2023-08-03 15:08:53 +0600

    further validations incorporated

--- backend/src/middlewares/validators/ads.ts
+++ backend/src/middlewares/validators/ads.ts
@@ -11,7 +11,7 @@ const prisma = new PrismaClient();
 
 
 // check if category exists
-const category = check('category').custom(async (value: string) => {
+const category = check('category_name').custom(async (value: string) => {
     const category = await prisma.category.findUnique({
         where: { name: value },
     });
@@ -22,5 +22,37 @@ const category = check('category').custom(async (value: string) => {
 })
 
 
+// check if title length is valid
+const title = check('title').isLength({ min: 5, max: 50 })
+                            .withMessage('Title length should be between 5 and 50 characters.')
 
-export const postAdValidation = [category]
+
+// check if description length is valid. Description is optional
+const description = check('description').optional().isLength({ min: 5, max: 500 })
+                            .withMessage('Description length should be between 5 and 500 characters.')
+
+
+// check if price is valid. Price is optional
+const price = check('price').optional().isNumeric()
+                            .withMessage('Price should be a number.')
+
+
+// check if is_negotiable is valid. is_negotiable is optional and defaults to false
+const is_negotiable = check('is_negotiable').optional().isBoolean()
+                            .withMessage('is_negotiable should be true/false.')
+
+
+// check if is_used is valid. is_used is optional and defaults to true
+const is_used = check('is_used').optional().isBoolean()
+                            .withMessage('is_used should be true/false.')
+
+
+// check if days_used is valid. days_used is optional
+const days_used = check('days_used').optional().isInt()
+                            .withMessage('days_used should be an integer.')
+
+
+
+
+
+export const postAdValidation = [category, title, description, price, is_negotiable, is_used, days_used]

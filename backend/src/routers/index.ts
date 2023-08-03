f8eddd6cf299f93e7f02733b380743dc10047c6c
Sheam1685 <samia12676@gmail.com>
2023-08-03 10:46:43 +0600

    router for ads created

--- backend/src/routers/index.ts
+++ backend/src/routers/index.ts
@@ -5,10 +5,12 @@ const router = express.Router();
 //import routers
 import authRouter from "./auth/auth";
 import thingsRouter from "./things/things"
+import adsRouter from "./ads/ads"
 
 
 // initialize routers
 router.use("/auth", authRouter);
 router.use("/things", thingsRouter);
+router.use("/ads", adsRouter);
 
 export default router;

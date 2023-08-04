cdbbdfcb36b2a9f63ca8f6b413ab86a50d5bad62
ansakib <1705107@ugrad.cse.buet.ac.bd>
2023-08-05 00:35:19 +0600

    code clean

--- backend/src/controllers/ads.ts
+++ backend/src/controllers/ads.ts
@@ -243,66 +243,7 @@ let get_ads = async (req:Request, res:Response) => {
     }
 
 }
-// let get_ads = async (req:Request, res:Response) => {
-//     const search_string = req.query.search_string;
 
-//     try {
-//         let ad_list = [];
-
-//         // if search_string is present, search for ads containing search_string in title or description
-//         if(search_string) {
-//             ad_list = await prisma.ads.findMany({
-//                 where: {
-//                     OR: [
-//                         { title: { 
-//                             contains: String(search_string),
-//                             mode: 'insensitive' 
-//                         } },
-//                         { description: { 
-//                             contains: String(search_string),
-//                             mode: 'insensitive'
-//                          } },
-//                     ]
-//                 },
-//                 select: {
-//                     id: true,
-//                     op_username: true,
-//                     category_name: true,
-//                     title: true,
-//                     price: true,
-//                     is_negotiable: true,
-//                     is_used: true,
-//                     promotion_type: true,
-//                     createdAt: true,
-//                 }
-//             });
-//         }
-
-//         // else, fetch all ads
-//         else{
-//             ad_list = await prisma.ads.findMany({
-//                 select: {
-//                     id: true,
-//                     op_username: true,
-//                     category_name: true,
-//                     title: true,
-//                     price: true,
-//                     is_negotiable: true,
-//                     is_used: true,
-//                     promotion_type: true,
-//                     createdAt: true,
-//                 }
-//             });
-//         }
-        
-//         return res.json(ad_list);
-//     } catch (error: any) {
-//         return res.status(500).json({
-//             success: false,
-//             error: error.message
-//         });
-//     }
-// }
 
 
 

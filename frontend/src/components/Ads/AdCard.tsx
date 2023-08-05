22d1a71062ee2c0aa7f77d0000259c46ccf570a6
ansakib <1705107@ugrad.cse.buet.ac.bd>
2023-08-05 20:03:31 +0600

    with props done except align

--- frontend/src/components/Ads/AdCard.tsx
+++ frontend/src/components/Ads/AdCard.tsx
@@ -1,30 +1,48 @@
-import { Card, CardBody, Image, Stack, Heading, Text } from "@chakra-ui/react";
+import {
+  Card,
+  CardBody,
+  Image,
+  Stack,
+  Heading,
+  Text,
+  Box,
+} from "@chakra-ui/react";
+import { FunctionComponent } from "react";
 
-function AdCard() {
-  return (
-    <Card
-      direction={{ base: "column", sm: "row" }}
-      overflow="hidden"
-      variant="outline"
-    >
-      <Image
-        objectFit="cover"
-        maxW={{ base: "100%", sm: "200px" }}
-        src="https://placehold.co/600x400"
-        alt="Caffe Latte"
-      />
-
-      <Stack>
-        <CardBody>
-          <Heading size="md">Galaxy A52 (Used)</Heading>
-          {/* Category */}
-          <Text py={2}>Mobile Phone</Text>
-          {/* Price */}
-          <Text>20000 Tk (Negotiable)</Text>
-        </CardBody>
-      </Stack>
-    </Card>
-  );
+interface AdCardProps {
+  title: string;
+  category: string;
+  price: string;
 }
+
+const AdCard: FunctionComponent<AdCardProps> = ({ title, category, price }) => {
+  return (
+    <Box height={"40"} width={"xl"} justifyItems={"end"} alignContent={"end"}>
+      <Card
+        direction={{ base: "column", sm: "row" }}
+        overflow="hidden"
+        variant="outline"
+        colorScheme="teal"
+      >
+        <Image
+          objectFit="cover"
+          maxW={{ base: "100%", sm: "200px" }}
+          src="https://placehold.co/600x400"
+          alt="Caffe Latte"
+        />
+
+        <Stack>
+          <CardBody>
+            <Heading size="md">{title}(Used)</Heading>
+            {/* Category */}
+            <Text py={2}>{category}</Text>
+            {/* Price */}
+            <Text>{price} Tk (Negotiable)</Text>
+          </CardBody>
+        </Stack>
+      </Card>
+    </Box>
+  );
+};
 
 export default AdCard;
--- frontend/src/components/Ads/GetAds.tsx
+++ frontend/src/components/Ads/GetAds.tsx
@@ -1,11 +1,59 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import AdCard from "./AdCard.tsx";
+import { VStack } from "@chakra-ui/react";
+
+const ads = [
+  {
+    id: "1",
+    title: "react tshirt",
+    category: "category",
+    imageSrc: "https://picsum.photos/seed/picsum6/420/260",
+    price: "$23.00",
+  },
+  {
+    id: "2",
+    title: "chakraUI mug",
+    category: "category",
+    imageSrc: "https://picsum.photos/seed/picsum5/421/261",
+    price: "$15.00",
+  },
+  {
+    id: "3",
+    title: "black tshirt",
+    category: "category",
+    imageSrc: "https://picsum.photos/seed/picsum4/422/262",
+    price: "$10.25",
+  },
+  {
+    id: "4",
+    title: "react tshirt",
+    category: "category",
+    imageSrc: "https://picsum.photos/seed/picsum3/420/260",
+    price: "$23.00",
+  },
+  {
+    id: "5",
+    title: "chakraUI mug",
+    category: "category",
+    imageSrc: "https://picsum.photos/seed/picsum2/421/261",
+    price: "$15.00",
+  },
+  {
+    id: "6",
+    title: "black tshirt",
+    category: "category",
+    imageSrc: "https://picsum.photos/seed/picsum1/422/262",
+    price: "$10.25",
+  },
+];
 
 function GetAds() {
   return (
-    <Link to="/things/1">
-      <AdCard />
-    </Link>
+    <VStack spacing={"0"}>
+      {ads.map((p) => (
+        <AdCard key={p.id} {...p} />
+      ))}
+    </VStack>
   );
 }
 

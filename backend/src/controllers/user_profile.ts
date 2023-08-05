4aa0eb0d8e8a3283fb9fc6bc4ca09069e7dc9add
ansakib <1705107@ugrad.cse.buet.ac.bd>
2023-08-05 10:36:31 +0600

    User Update done

--- backend/src/controllers/user_profile.ts
+++ backend/src/controllers/user_profile.ts
@@ -50,6 +50,37 @@ let get_user_profile = async (req: Request, res: Response) => {
 let update_user_profile = async (req: Request, res: Response) => {
     const user: any = req.user;
     
+    // update user profile in db [field: name, phone, dob]
+    try{
+        const updatedUserProfile = await prisma.users.update({
+            where: { username: user.username },
+            data: {
+                name: req.body.name,
+                phone: req.body.phone,
+                dob: new Date(req.body.dob)
+            }
+        });
+
+        // user not found
+        if (!updatedUserProfile) {
+            return res.status(404).json({
+                success: false,
+                error: 'User not found!'
+            });
+        }
+
+        // return user profile
+        return res.status(200).json({
+            success: true,
+            message: 'Profile updated successfully!'
+        })
+    }
+    catch(error: any) {
+        return res.status(500).json({
+            success: false,
+            error: error.message
+        });
+    }
 }
 
 export { get_user_profile, update_user_profile }
--- backend/src/routers/user/profile.ts
+++ backend/src/routers/user/profile.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { userAuth } from "../../middlewares/auth-middleware";
 import { get_user_profile, update_user_profile } from "../../controllers/user_profile";
 import { update_user_validation } from "../../middlewares/validators/update_user";
+import { validationMiddleware } from "../../middlewares/validations-middleware";
 
 
 const router = express.Router();
@@ -11,7 +12,7 @@ const router = express.Router();
 // get user profile: /api/profile
 router.route("/")
     .get(userAuth, get_user_profile)
-    .put(userAuth,update_user_validation, update_user_profile)
+    .put(userAuth,update_user_validation, validationMiddleware, update_user_profile)
 
 
 export default router;

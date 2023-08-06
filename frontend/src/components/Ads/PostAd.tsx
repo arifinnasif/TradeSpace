5ece0f687ce479852913fcf740e31ebcd1d692fa
Sheam1685 <samia12676@gmail.com>
2023-08-06 19:00:10 +0600

    ad post form modified

--- frontend/src/components/Ads/PostAd.tsx
+++ frontend/src/components/Ads/PostAd.tsx
@@ -1,5 +1,8 @@
-import { Box, ButtonGroup, Flex } from "@chakra-ui/react";
-import { Formik } from "formik";
+//@ts-nocheck
+//@ts-ignore
+
+import { Box, Button, ButtonGroup, Flex } from "@chakra-ui/react";
+import { Formik, useFormik } from "formik";
 
 import {
   CheckboxSingleControl,
@@ -10,63 +13,136 @@ import {
   TextareaControl,
 } from "formik-chakra-ui";
 
+import React, {useState, useEffect} from 'react'
 
 import * as Yup from "yup";
 
-
-const onSubmit = (values:any) => {
-    console.log(JSON.stringify(values, null, 2));
-};
-
-
-
-const categories = [
-    "Electronics",
-    "Furniture",
-    "Clothing",
-    "Books",
-]
-
-
-
-const phone = 12345678912;
-
+const onAdd = ({category, 
+                title, 
+                is_sell_ad, 
+                description, 
+                is_negotiable, 
+                is_used, 
+                days_used, 
+                phone, 
+                is_phone_public, 
+                address, 
+                price, 
+                images}) => {
+  const newAd = {category, title, is_sell_ad, description, is_negotiable, is_used, days_used, phone, is_phone_public, address, price, images}
+  console.log(newAd)
+}
 
 
 const initialValues = {
-    select_category: "",
-    title: "",
-    is_sell_ad: false,
-    description: "",
-    is_negotiable: false,
-    is_used: false,
-    phone: phone,
-    days_used: "",
-    is_phone_public: false,
-    address: "",
-    price: "",
-    images: [],
+  select_category: "",
+  title: "",
+  is_sell_ad: false,
+  description: "",
+  is_negotiable: false,
+  is_used: false,
+  phone: '12345678912',
+  days_used: "",
+  is_phone_public: false,
+  address: "",
+  price: "",
+  images: [],
 };
 
-
+const categories = [
+  "Electronics",
+  "Furniture",
+  "Clothing",
+]
 
 const validationSchema = Yup.object({
-  title: Yup.string().required().min(5).max(50),
-  is_sell_ad: Yup.boolean(),
-  description: Yup.string().max(100),
-  price: Yup.number().required().min(0),
-  is_negotiable: Yup.boolean(),
-  is_used: Yup.boolean(),
-  days_used: Yup.number().min(0),
-  phone: Yup.number().required().min(10000000000).max(99999999999),
-  is_phone_public: Yup.boolean(),
-  address: Yup.string().required().min(5).max(50),
-  images: Yup.array().max(5),
+title: Yup.string().required().min(5).max(50),
+is_sell_ad: Yup.boolean(),
+description: Yup.string().max(100),
+price: Yup.number().required().min(0),
+is_negotiable: Yup.boolean(),
+is_used: Yup.boolean(),
+days_used: Yup.number().min(0),
+phone: Yup.number().required().min(10000000000).max(99999999999),
+is_phone_public: Yup.boolean(),
+address: Yup.string().required().min(5).max(50),
+images: Yup.array().max(5),
 });
 
-
-
 const PostAd = () => {
+  const [category, setCategory] = useState('')
+  const [title, setTitle] = useState('')
+  const [is_sell_ad, setIs_sell_ad] = useState(false)
+  const [description, setDescription] = useState('')
+  const [is_negotiable, setIs_negotiable] = useState(false)
+  const [is_used, setIs_used] = useState(false)
+  const [days_used, setDays_used] = useState('')
+  const [phone, setPhone] = useState(12345678912)
+  const [is_phone_public, setIs_phone_public] = useState(false)
+  const [address, setAddress] = useState('')
+  const [price, setPrice] = useState('')
+  const [images, setImages] = useState([])
+
+
+  const changeCategory = (e) => {
+    setCategory(e.target.value)
+  }
+  const changeTitle = (e) => {
+    setTitle(e.target.value)
+  }
+  const changeIs_sell_ad = (e) => {
+    setIs_sell_ad(!is_sell_ad)
+  }
+  const changeDescription = (e) => {
+    setDescription(e.target.value)
+  }
+  const changeIs_negotiable = (e) => {
+    setIs_negotiable(!is_negotiable)
+  }
+  const changeIs_used = (e) => {
+    setIs_used(!is_used)
+  }
+  const changeDays_used = (e) => {
+    setDays_used(e.target.value)
+  }
+  const changePhone = (e) => {
+    setPhone(e.target.value)
+  }
+  const changeIs_phone_public = (e) => {
+    setIs_phone_public(!is_phone_public)
+  }
+  const changeAddress = (e) => {
+    setAddress(e.target.value)
+  }
+  const changePrice = (e) => {
+    setPrice(e.target.value)
+  }
+  const changeImages = (e) => {
+    setImages(e.target.value)
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    // if (!text) {
+    //   alert('Please add a task')
+    //   return
+    // }
+    onAdd({category, 
+           title, 
+           is_sell_ad, 
+           description, 
+           is_negotiable, 
+           is_used, 
+           days_used, 
+           phone, 
+           is_phone_public, 
+           address, 
+           price, 
+           images
+          })
+
+  }
+
   return (
     <Formik
       initialValues={initialValues}
@@ -74,21 +150,20 @@ const PostAd = () => {
       validationSchema={validationSchema}
     >
       {({ handleSubmit, values, errors, setFieldValue }) => (
-        
         <Box
-          borderWidth="1px"
-          rounded="lg"
-          shadow="1px 1px 3px rgba(0,0,0,0.3)"
-          maxW={'75vw'}
-          p={6}
-          m="10px auto"
-          as="form"
-          onSubmit={handleSubmit as any}
+        borderWidth="1px"
+        rounded="lg"
+        shadow="1px 1px 3px rgba(0,0,0,0.3)"
+        maxW={'75vw'}
+        p={6}
+        m="10px auto"
+        as="form"
+        onSubmit={handleSubmit as any}
         >
-
           <SelectControl
-            name="select_category"
+            name="category"
             selectProps={{ placeholder: "Select Category" }}
+            onChange={changeCategory}
           >
           {categories.map((category, index) => (
           <option key={index} value={category}>
@@ -96,33 +171,31 @@ const PostAd = () => {
           </option>
           ))}
           </SelectControl>
-
           <br />
           
-          <InputControl name="tite" label="Title" />
+          <InputControl name="title" label="Title" onChange={changeTitle} />
           
           <br />
-          
-          <CheckboxSingleControl name="is_sell_ad">
+          <CheckboxSingleControl name="is_sell_ad" onChange={changeIs_sell_ad}>
             Is a sell Ad
           </CheckboxSingleControl>
           
           <br />
-          
-          <TextareaControl name="description" label="Description(max 100 characters)" />
-          
-          <br />
-          
-          <InputControl name="price" label="Price" />
+
+          <TextareaControl name="description" label="Description(max 100 characters)" onChange={changeDescription}/>
           
           <br />
           
+          <InputControl name="price" label="Price" onChange={changePrice}/>
+          
+          <br />
+
           <Flex>
-          <CheckboxSingleControl name="is_negotiable">
+          <CheckboxSingleControl name="is_negotiable" onChange={changeIs_negotiable}>
             Price is negotiable
           </CheckboxSingleControl>
           
-          <CheckboxSingleControl name="is_used">
+          <CheckboxSingleControl name="is_used" onChange={changeIs_used}>
             Product is used
           </CheckboxSingleControl>
           </Flex>
@@ -140,41 +213,39 @@ const PostAd = () => {
             name="images"
             accept="image/*"
             multiple
-            onChange={(event) => {
-              setFieldValue("images", event.currentTarget.files);
-            }}
+            onChange={changeImages}
           />
 
           <br />
           <br />
           
-          <InputControl name="days_used" label="Days Used(N/A for unused product)" isDisabled={!values.is_used}/>
+          <InputControl name="days_used" label="Days Used(N/A for unused product)" isDisabled={!is_used} onChange={changeDays_used}/>
+    
           
           <br />
           
-          <InputControl name="phone" label="Phone" />
+          <InputControl name="phone" label="Phone" onChange={changePhone}/>
           
           <br />
-          
-          <CheckboxSingleControl name="is_phone_public">
+
+          <CheckboxSingleControl name="is_phone_public" onChange={changeIs_phone_public}>
             Phone Number is public
           </CheckboxSingleControl>
           
           <br />
           
-          <InputControl name="address" label="Address" />
+          <InputControl name="address" label="Address" onChange={changeAddress}/>
 
           <br />
-          
+
           <ButtonGroup>
-            <SubmitButton>Submit</SubmitButton>
+            <Button type="submit" onClick={onSubmit}>Submit</Button>
             <ResetButton>Reset</ResetButton>
           </ButtonGroup>
-        
         </Box>
       )}
     </Formik>
-  );
-};
+  )
+}
 
-export default PostAd;
+export default PostAd

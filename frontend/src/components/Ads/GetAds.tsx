3bf4fcaef4908621403aa405b9d77880c99519da
ansakib <1705107@ugrad.cse.buet.ac.bd>
2023-08-06 13:18:32 +0600

    Home service category done

--- frontend/src/components/Ads/GetAds.tsx
+++ frontend/src/components/Ads/GetAds.tsx
@@ -6,69 +6,6 @@ import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { adService, AdCardType } from "../../services/ad.service";
 
-// const ads = [
-//   {
-//     id: "1",
-//     title: "React tshirt",
-//     category: "Category",
-//     imageSrc: "https://picsum.photos/seed/picsum6/420/260",
-//     price: "$23.00",
-//     is_used: true,
-//     is_negotiable: false,
-//     is_sell_ad: false,
-//   },
-//   {
-//     id: "2",
-//     title: "chakraUI mug",
-//     category: "category",
-//     imageSrc: "https://picsum.photos/seed/picsum5/421/261",
-//     price: "$15.00",
-//     is_used: true,
-//     is_negotiable: true,
-//     is_sell_ad: true,
-//   },
-//   {
-//     id: "3",
-//     title: "black tshirt",
-//     category: "category",
-//     imageSrc: "https://picsum.photos/seed/picsum4/422/262",
-//     price: "$10.25",
-//     is_used: false,
-//     is_negotiable: true,
-//     is_sell_ad: false,
-//   },
-//   {
-//     id: "4",
-//     title: "react tshirt",
-//     category: "category",
-//     imageSrc: "https://picsum.photos/seed/picsum3/420/260",
-//     price: "$23.00",
-//     is_used: true,
-//     is_negotiable: true,
-//     is_sell_ad: false,
-//   },
-//   {
-//     id: "5",
-//     title: "chakraUI mug",
-//     category: "category",
-//     imageSrc: "https://picsum.photos/seed/picsum2/421/261",
-//     price: "$15.00",
-//     is_used: false,
-//     is_negotiable: true,
-//     is_sell_ad: true,
-//   },
-//   {
-//     id: "6",
-//     title: "black tshirt",
-//     category: "category",
-//     imageSrc: "https://picsum.photos/seed/picsum1/422/262",
-//     price: "$10.25",
-//     is_used: true,
-//     is_negotiable: false,
-//     is_sell_ad: false,
-//   },
-// ];
-
 function GetAds() {
   const [isLoading, setIsLoading] = useState(false);
   const [adsList, setAdsList] = useState<AdCardType[]>();

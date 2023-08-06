3ccbb19123e7832bd08b6a39f733e50531a7f4cb
Sheam1685 <samia12676@gmail.com>
2023-08-06 12:19:43 +0600

    demo navbar added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,11 +6,13 @@ import Things from "./components/Things";
 import GetAds from "./components/Ads/GetAds";
 import Register from "./components/Register/RegisterSteps";
 import PostAd from "./components/Ads/PostAd";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
       <Suspense fallback={<Spinner />}>
+        <Navbar />
         <Routes>
           <Route path="/things/:id/" element={<Things />} />
           <Route path="/ads/" element={<GetAds />} />

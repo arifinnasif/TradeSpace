3849736682056f0972eb43427ace3a5bef5a5ceb
ansakib <1705107@ugrad.cse.buet.ac.bd>
2023-08-06 16:44:52 +0600

    Search Component with frontend works

--- frontend/src/components/Homepage/SearchBar.tsx
+++ frontend/src/components/Homepage/SearchBar.tsx
@@ -1,29 +1,33 @@
-import React, { ChangeEvent } from "react";
-import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import React, { ChangeEvent, useState } from "react";
+import { Button, Input, InputGroup, InputRightElement } from "@chakra-ui/react";
 import { SearchIcon } from "@chakra-ui/icons";
+import { SubmitButton } from "formik-chakra-ui";
+import { Link } from "react-router-dom";
 
-interface SearchComponentProps {
-  onSearch: (searchTerm: string) => void;
-}
-
-const SearchComponent: React.FC<SearchComponentProps> = ({ onSearch }) => {
+const SearchComponent: React.FC = () => {
+  const [searchTerm, setSearchTerm] = useState("");
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const searchTerm = event.target.value;
-    onSearch(searchTerm);
+    setSearchTerm(event.target.value);
   };
 
   return (
     <InputGroup>
-      <InputLeftElement
-        pointerEvents="none"
-        children={<SearchIcon color="gray.300" />}
-      />
       <Input
         type="text"
         placeholder="Search..."
         onChange={handleInputChange}
         focusBorderColor="teal.400"
       />
+      <Link to={`/ads/?search_string=${searchTerm}`}>
+        <Button
+          type="submit"
+          colorScheme="teal"
+          variant="outline"
+          ml="2"
+          size="md"
+          leftIcon={<SearchIcon />}
+        />
+      </Link>
     </InputGroup>
   );
 };

57e2e751307af7d147823148e29374c7f23503ca
ansakib <1705107@ugrad.cse.buet.ac.bd>
2023-08-06 15:59:17 +0600

    formik reform

--- frontend/src/components/Ads/PostAd.tsx
+++ frontend/src/components/Ads/PostAd.tsx
@@ -10,45 +10,34 @@ import {
   TextareaControl,
 } from "formik-chakra-ui";
 
-
 import * as Yup from "yup";
 
+const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
 
-const onSubmit = (values:any) => {
-    console.log(JSON.stringify(values, null, 2));
+const onSubmit = (values: any) => {
+  return sleep(300).then(() => {
+    alert(JSON.stringify(values, null, 2));
+  });
 };
 
-
-
-const categories = [
-    "Electronics",
-    "Furniture",
-    "Clothing",
-    "Books",
-]
-
-
+const categories = ["Electronics", "Furniture", "Clothing", "Books"];
 
 const phone = 12345678912;
 
-
-
 const initialValues = {
-    select_category: "",
-    title: "",
-    is_sell_ad: false,
-    description: "",
-    is_negotiable: false,
-    is_used: false,
-    phone: phone,
-    days_used: "",
-    is_phone_public: false,
-    address: "",
-    price: "",
-    images: [],
+  category: "",
+  title: "",
+  is_sell_ad: false,
+  description: "",
+  is_negotiable: false,
+  is_used: false,
+  phone: phone,
+  days_used: "",
+  is_phone_public: false,
+  address: "",
+  price: "",
+  images: [],
 };
-
-
 
 const validationSchema = Yup.object({
   title: Yup.string().required().min(5).max(50),
@@ -64,8 +53,6 @@ const validationSchema = Yup.object({
   images: Yup.array().max(5),
 });
 
-
-
 const PostAd = () => {
   return (
     <Formik
@@ -74,67 +61,68 @@ const PostAd = () => {
       validationSchema={validationSchema}
     >
       {({ handleSubmit, values, errors, setFieldValue }) => (
-        
         <Box
           borderWidth="1px"
           rounded="lg"
           shadow="1px 1px 3px rgba(0,0,0,0.3)"
-          maxW={'75vw'}
+          maxW={"75vw"}
           p={6}
           m="10px auto"
           as="form"
           onSubmit={handleSubmit as any}
         >
-
           <SelectControl
-            name="select_category"
+            name="category"
             selectProps={{ placeholder: "Select Category" }}
           >
-          {categories.map((category, index) => (
-          <option key={index} value={category}>
-          {category}
-          </option>
-          ))}
+            {categories.map((category, index) => (
+              <option key={index} value={category}>
+                {category}
+              </option>
+            ))}
           </SelectControl>
 
           <br />
-          
-          <InputControl name="tite" label="Title" />
-          
+
+          <InputControl name="title" label="Title" />
+
           <br />
-          
+
           <CheckboxSingleControl name="is_sell_ad">
             Is a sell Ad
           </CheckboxSingleControl>
-          
+
           <br />
-          
-          <TextareaControl name="description" label="Description(max 100 characters)" />
-          
+
+          <TextareaControl
+            name="description"
+            label="Description(max 100 characters)"
+          />
+
           <br />
-          
+
           <InputControl name="price" label="Price" />
-          
+
           <br />
-          
+
           <Flex>
-          <CheckboxSingleControl name="is_negotiable">
-            Price is negotiable
-          </CheckboxSingleControl>
-          
-          <CheckboxSingleControl name="is_used">
-            Product is used
-          </CheckboxSingleControl>
+            <CheckboxSingleControl name="is_negotiable">
+              Price is negotiable
+            </CheckboxSingleControl>
+
+            <CheckboxSingleControl name="is_used">
+              Product is used
+            </CheckboxSingleControl>
           </Flex>
 
           <br />
 
           {/* take 5 images as input */}
           <label>Images (up to 5)</label>
-          
+
           <br />
           <br />
-          
+
           <input
             type="file"
             name="images"
@@ -147,30 +135,33 @@ const PostAd = () => {
 
           <br />
           <br />
-          
-          <InputControl name="days_used" label="Days Used(N/A for unused product)" isDisabled={!values.is_used}/>
-          
+
+          <InputControl
+            name="days_used"
+            label="Days Used(N/A for unused product)"
+            isDisabled={!values.is_used}
+          />
+
           <br />
-          
+
           <InputControl name="phone" label="Phone" />
-          
+
           <br />
-          
+
           <CheckboxSingleControl name="is_phone_public">
             Phone Number is public
           </CheckboxSingleControl>
-          
+
           <br />
-          
+
           <InputControl name="address" label="Address" />
 
           <br />
-          
+
           <ButtonGroup>
-            <SubmitButton>Submit</SubmitButton>
+            <SubmitButton onClick={onSubmit(values)}>Submit</SubmitButton>
             <ResetButton>Reset</ResetButton>
           </ButtonGroup>
-        
         </Box>
       )}
     </Formik>

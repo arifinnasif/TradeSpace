34023ce3938367dab18a608339660cec3a2361d8
Sultanul Arifin <msa336@outlook.com>
2023-08-06 23:38:22 +0600

    registration step 3 form done

--- frontend/src/components/Register/RegisterStep3Components.tsx
+++ frontend/src/components/Register/RegisterStep3Components.tsx
@@ -2,28 +2,47 @@
 
 import { FormControl, FormLabel, Input, Button } from "@chakra-ui/react";
 
-import { Link } from "react-router-dom";
+import validator from "validator";
 
-const Step3 = () => {
+import { Link } from "react-router-dom";
+import { FormEvent, FunctionComponent } from "react";
+
+interface Step3Props {
+  otp?: string;
+  setOtp: (otp: string) => void;
+}
+
+const Step3: FunctionComponent<Step3Props> = ({ otp, setOtp }) => {
+  const isCurrentInputInValid = () => {
+    return otp == undefined || isOtpInValid();
+  };
+  const isOtpInValid = () => {
+    if (otp == undefined) return false;
+    if (otp.length !== 6) return true;
+    return !validator.isNumeric(otp);
+  };
+  const handleOtpChange = (e: FormEvent<HTMLInputElement>) => {
+    const tmp = e.currentTarget.value;
+    setOtp(tmp);
+  };
   return (
     <>
-      <FormControl id="otp">
+      <FormControl id="otp" isInvalid={isOtpInValid()}>
         <FormLabel>Verification OTP:</FormLabel>
-        <Input type="number" />
+        <Input type="number" value={otp} onChange={handleOtpChange} />
       </FormControl>
 
-      <Link to="/">
-        <Button
-          w={"full"}
-          bg={"teal.400"}
-          color={"white"}
-          _hover={{
-            bg: "blue.500",
-          }}
-        >
-          Login
-        </Button>
-      </Link>
+      <Button
+        isDisabled={isCurrentInputInValid()}
+        w={"full"}
+        bg={"teal.400"}
+        color={"white"}
+        _hover={{
+          bg: "blue.500",
+        }}
+      >
+        Verify
+      </Button>
     </>
   );
 };

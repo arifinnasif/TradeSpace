c5dd453e1770cdbcdc7a750191fdde63bbabf38a
ansakib <1705107@ugrad.cse.buet.ac.bd>
2023-08-10 22:16:58 +0600

    Login page added

---
+++ frontend/src/components/Login/Login.tsx
@@ -0,0 +1,61 @@
+import {
+  Box,
+  Button,
+  Checkbox,
+  Container,
+  FormControl,
+  FormLabel,
+  Heading,
+  HStack,
+  Input,
+  Link,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
+import { Logo } from "./Logo";
+import { PasswordField } from "./PasswordField";
+
+export const App = () => (
+  <Container
+    maxW="lg"
+    py={{ base: "12", md: "24" }}
+    px={{ base: "0", sm: "8" }}
+  >
+    <Stack spacing="8">
+      <Stack spacing="6">
+        <Logo />
+        <Stack spacing={{ base: "2", md: "3" }} textAlign="center">
+          <Heading size={{ base: "xs", md: "sm" }}>
+            Log in to your account
+          </Heading>
+          <Text color="fg.muted">
+            Don't have an account? <Link href="#">Sign up</Link>
+          </Text>
+        </Stack>
+      </Stack>
+      <Box
+        py={{ base: "0", sm: "8" }}
+        px={{ base: "4", sm: "10" }}
+        bg={{ base: "transparent", sm: "bg.surface" }}
+        boxShadow={{ base: "none", sm: "md" }}
+        borderRadius={{ base: "none", sm: "xl" }}
+      >
+        <Stack spacing="6">
+          <Stack spacing="5">
+            <FormControl>
+              <FormLabel htmlFor="email">Email</FormLabel>
+              <Input id="email" type="email" />
+            </FormControl>
+            <PasswordField />
+          </Stack>
+          <HStack justify="space-between">
+            <Checkbox defaultChecked>Remember me</Checkbox>
+            <Button variant="text" size="sm">
+              Forgot password?
+            </Button>
+          </HStack>
+        </Stack>
+      </Box>
+    </Stack>
+  </Container>
+);

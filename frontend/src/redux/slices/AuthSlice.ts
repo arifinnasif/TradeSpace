221ad6fd597a60abbd62c68c03274213a21a4a6a
ansakib <1705107@ugrad.cse.buet.ac.bd>
2023-08-11 00:37:08 +0600

    authslice changed

--- frontend/src/redux/slices/AuthSlice.ts
+++ frontend/src/redux/slices/AuthSlice.ts
@@ -1,17 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const userAuthFromLocalStorage = () => {
-  const isAuth = localStorage.getItem("isAuth");
-
-  if (isAuth && JSON.parse(isAuth) === true) {
-    return true;
-  }
-
-  return false;
-};
-
 const initialState = {
-  isAuth: userAuthFromLocalStorage(),
+  isAuth: false,
 };
 
 export const authSlice = createSlice({

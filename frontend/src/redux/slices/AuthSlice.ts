73cc54df050775f8329926197efa46b58acd166b
ansakib <1705107@ugrad.cse.buet.ac.bd>
2023-08-10 22:48:06 +0600

    auth slice init

--- frontend/src/redux/slices/AuthSlice.ts
+++ frontend/src/redux/slices/AuthSlice.ts
@@ -1,32 +1,15 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
-export interface CounterState {
-  value: number;
-  incrementAmount: number;
-}
+const userAuthFromLocalStorage = () => {
+  const isAuth = localStorage.getItem("isAuth");
 
-const initialState: CounterState = {
-  value: 0,
-  incrementAmount: 1,
+  if (isAuth && JSON.parse(isAuth) === true) {
+    return true;
+  }
+
+  return false;
 };
 
-export const counterSlice = createSlice({
-  name: "counter",
-  initialState,
-  reducers: {
-    increment: (state) => {
-      state.value += state.incrementAmount;
-    },
-    decrement: (state) => {
-      state.value -= state.incrementAmount;
-    },
-    changeIncrementAmount: (state, action: PayloadAction<number>) => {
-      state.incrementAmount = action.payload;
-    },
-  },
-});
-
-export const { increment, decrement, changeIncrementAmount } =
-  counterSlice.actions;
-
-export default counterSlice.reducer;
+const initialState = {
+  isAuth: userAuthFromLocalStorage(),
+};

0275d86046206317f5b2f4e34a9852f3e795f808
ansakib <1705107@ugrad.cse.buet.ac.bd>
2023-08-11 01:30:05 +0600

    login service

---
+++ frontend/src/services/login.service.ts
@@ -0,0 +1,9 @@
+import API from "../api/axios.config";
+
+class LoginService {
+  async login(userinfo: any) {
+    return await API.post(`/auth/login`, userinfo, { withCredentials: true });
+  }
+}
+
+export const loginService = new LoginService();

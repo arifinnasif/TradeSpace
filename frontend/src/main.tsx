3df576ffaa155349e0360c1f2a7d9f8e8c2123b3
ansakib <1705107@ugrad.cse.buet.ac.bd>
2023-08-10 22:38:05 +0600

    Redux connection

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,8 @@ import "./index.css";
 import custom_theme from "./utils/theme.ts";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { Helmet, HelmetProvider } from "react-helmet-async";
+import { Provider } from "react-redux";
+import store from "./redux/store.ts";
 
 // 2. Extend the theme to include custom colors, fonts, etc
 
@@ -14,7 +16,9 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <HelmetProvider>
       <ChakraProvider theme={theme}>
-        <App />
+        <Provider store={store}>
+          <App />
+        </Provider>
       </ChakraProvider>
     </HelmetProvider>
   </React.StrictMode>

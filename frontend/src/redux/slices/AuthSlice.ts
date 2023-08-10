8c92cd32a80dca2a28c044f889a75fc4dfff1176
ansakib <1705107@ugrad.cse.buet.ac.bd>
2023-08-10 22:48:28 +0600

    authSlice done

--- frontend/src/redux/slices/AuthSlice.ts
+++ frontend/src/redux/slices/AuthSlice.ts
@@ -13,3 +13,20 @@ const userAuthFromLocalStorage = () => {
 const initialState = {
   isAuth: userAuthFromLocalStorage(),
 };
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    authenticateUser: (state) => {
+      state.isAuth = true;
+    },
+    unauthenticateUser: (state) => {
+      state.isAuth = false;
+    },
+  },
+});
+
+export const { authenticateUser, unauthenticateUser } = authSlice.actions;
+
+export default authSlice.reducer;

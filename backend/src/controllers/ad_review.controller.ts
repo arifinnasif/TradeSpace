570dcc6b300e0726b0b3cb55db9bcf2e621daaac
Sultanul Arifin <msa336@outlook.com>
2023-08-12 00:12:31 +0600

    ad review details reformatted

--- backend/src/controllers/ad_review.controller.ts
+++ backend/src/controllers/ad_review.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 import prisma from '../../prisma/prisma_client';
 import { notify_user } from './user_notification.controller';
+import _ from 'lodash';
 import * as dotenv from "dotenv";
 
 const limit = 10;
@@ -37,15 +38,102 @@ export const get_all_pending_reviews = async (req: Request, res: Response) => {
 
 export const get_pending_review_details = async (req: Request, res: Response) => {
     try {
-        const pending_review = await prisma.ads.findUnique({
-            where: {
-                id: Number(req.params.id!)
+        let review_details: {
+            id: number;
+            op_username: string;
+            op?: {
+                name: string;
+
+            };
+            category_name: string;
+            title: string;
+            description?: string | null;
+            price?: number | null;
+            is_negotiable: boolean;
+            is_used: boolean;
+            is_sell_ad: boolean;
+            is_phone_public: boolean;
+            days_used?: number | null;
+            address?: string | null;
+            promotion_type: string;
+            createdAt: Date;
+            status?: string;
+        } | null
+            = await prisma.ads.findUnique({
+                where: { id: Number(req.params.adId) },
+                select: {
+                    id: true,
+                    op_username: true,
+                    op: {
+                        select: {
+                            name: true
+                        },
+                    },
+                    category_name: true,
+                    title: true,
+                    description: true,
+                    price: true,
+                    is_negotiable: true,
+                    is_used: true,
+                    is_sell_ad: true,
+                    is_phone_public: true,
+                    days_used: true,
+                    address: true,
+                    promotion_type: true,
+                    createdAt: true,
+                    status: true,
+                }
+            });
+
+
+        // ad not found
+        if (!review_details || review_details.status !== 'pending') {
+            return res.status(404).json({
+                success: false,
+                error: 'review not found!'
+            });
+        }
+
+
+        let usage_time = undefined;
+        if (review_details.days_used !== undefined && review_details.days_used !== null) {
+            usage_time = {
+                years: Math.floor(review_details.days_used / 365),
+                months: Math.floor((review_details.days_used % 365) / 30),
+                days: Math.floor((review_details.days_used % 365) % 30),
             }
+        }
+
+
+
+        const user = await prisma.users.findUnique({
+            where: { username: review_details.op_username },
+            select: { phone: true }
         });
 
-        if (pending_review?.status !== 'pending') return res.status(404).json({});
 
-        return res.status(200).json(pending_review);
+
+        // capitalize
+        const op_fullname = _.startCase(review_details.op!.name);
+
+
+        // remove days_used from review_details
+        delete review_details.days_used;
+
+        // remove op from review_details
+        delete review_details.op;
+
+        // add usage_time and phone to review_details
+        const review_details_json = {
+            ...review_details,
+            usage_time: usage_time,
+            phone: user?.phone,
+            op_fullname: op_fullname,
+        }
+
+
+
+        return res.json(review_details_json);
     } catch (error: any) {
         return res.status(500).json({
             success: false,

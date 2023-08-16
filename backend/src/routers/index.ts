fcef94516eebc496fa4fd585ce302833cc98626c
Sultanul Arifin <msa336@outlook.com>
2023-08-16 20:19:44 +0600

    payment router added with bug fixes

--- backend/src/routers/index.ts
+++ backend/src/routers/index.ts
@@ -8,6 +8,7 @@ import thingsRouter from "./things/things"
 import adsRouter from "./ads/ads"
 import adminRouter from "./admin";
 import profileRouter from "./user/profile"
+import paymentRouter from "./payment/payment"
 
 
 
@@ -16,7 +17,8 @@ router.use("/auth", authRouter);
 router.use("/things", thingsRouter);
 router.use("/ads", adsRouter);
 router.use("/admin", adminRouter);
-router.use("/profile",  profileRouter);
+router.use("/profile", profileRouter);
+router.use("/payment", paymentRouter);
 
 
 export default router;

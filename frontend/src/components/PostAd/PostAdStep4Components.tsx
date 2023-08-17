e10e081d541a05275fb27d0ff1009dde305d26b5
Sheam1685 <samia12676@gmail.com>
2023-08-17 17:41:11 +0600

    preview of ad works

--- frontend/src/components/PostAd/PostAdStep4Components.tsx
+++ frontend/src/components/PostAd/PostAdStep4Components.tsx
@@ -65,9 +65,105 @@ const Step4: FunctionComponent<Step4Props> = ({
             {title}
           </Text>
         </Box>
+
+        <Box>
+          <Heading size='xs' textTransform='uppercase'>
+            Description
+          </Heading>
+          <Text pt='2' fontSize='sm'>
+            {description}
+          </Text>
+        </Box>
+
+        <Box>
+          <Heading size='xs' textTransform='uppercase'>
+            Type of Ad
+          </Heading>
+          <Text pt='2' fontSize='sm'>
+            {is_sell_ad ? 'Sell Ad' : 'Buy Ad'}
+          </Text>
+        </Box>
+
+        <Box>
+          <Heading size='xs' textTransform='uppercase'>
+            Price
+          </Heading>
+          <Text pt='2' fontSize='sm'>
+            {price}
+          </Text>
+        </Box>
+
+        <Box>
+          <Heading size='xs' textTransform='uppercase'>
+            Is Price Negotiable?
+          </Heading>
+          <Text pt='2' fontSize='sm'>
+            {is_negotiable ? 'Price is negotiable' : 'Price is not negotiable'}
+          </Text>
+        </Box>
+
+        <Box>
+          <Heading size='xs' textTransform='uppercase'>
+            Is Product Used?
+          </Heading>
+          <Text pt='2' fontSize='sm'>
+            {is_used ? 'Used Product' : 'New Product'}
+          </Text>
+        </Box>
+
+        <Box>
+          <Heading size='xs' textTransform='uppercase'>
+            Time of Use
+          </Heading>
+          <Text pt='2' fontSize='sm'>
+          {!is_used ? "N/A for new products" : (
+            <>
+              {years_used !== undefined && <span>{years_used} years</span>}
+              {months_used !== undefined && <span>{years_used ? ", " : ""}{months_used} months</span>}
+              {days_used !== undefined && <span>{(years_used || months_used) ? ", " : ""}{days_used} days</span>}
+            </>
+          )}
+          </Text>
+        </Box>
+
+        <Box>
+          <Heading size='xs' textTransform='uppercase'>
+            Images
+          </Heading>
+          <Text pt='2' fontSize='sm'>
+            {images && images.length > 0 ? (
+              images.map((image, index) => (
+                <span key={index}>{image}</span>
+              ))
+            ) : (
+              "No images provided"
+            )}
+          </Text>
+        </Box>
+
+        <Box>
+          <Heading size='xs' textTransform='uppercase'>
+            Is Phone Number Public?
+          </Heading>
+          <Text pt='2' fontSize='sm'>
+            {is_phone_public ? 'Phone number is public' : 'Phone number is private'}
+          </Text>
+        </Box>
+
+        <Box>
+          <Heading size='xs' textTransform='uppercase'>
+            Address
+          </Heading>
+          <Text pt='2' fontSize='sm'>
+            {address}
+          </Text>
+        </Box>
+
       </Stack>
     </CardBody>
   </Card>
+
+  
   )
 }
 

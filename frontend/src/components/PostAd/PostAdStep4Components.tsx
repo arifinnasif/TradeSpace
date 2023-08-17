364b198910e2e11af14ea1cd602e877ded1cab22
Sheam1685 <samia12676@gmail.com>
2023-08-17 18:05:49 +0600

    step 4 complete

--- frontend/src/components/PostAd/PostAdStep4Components.tsx
+++ frontend/src/components/PostAd/PostAdStep4Components.tsx
@@ -1,4 +1,4 @@
-import { Box, Card, CardBody, CardHeader, Heading, Stack,
+import { Box, Button, Card, CardBody, CardHeader, Flex, Heading, Stack,
          StackDivider,
          Text,
 } from "@chakra-ui/react";
@@ -41,129 +41,153 @@ const Step4: FunctionComponent<Step4Props> = ({
   address,
 }) => {
   return(
-  <Card>
-    <CardHeader>
-      <Heading size='md'>Check your Ad informations</Heading>
-    </CardHeader>
+    <>
+      <Card>
+        <CardHeader>
+          <Heading size='md'>Check your Ad informations</Heading>
+        </CardHeader>
 
-    <CardBody>
-      <Stack divider={<StackDivider />} spacing='4'>
-        <Box>
-          <Heading size='xs' textTransform='uppercase'>
-            Category
-          </Heading>
-          <Text pt='2' fontSize='sm'>
-            {category}
-          </Text>
-        </Box>
-        
-        <Box>
-          <Heading size='xs' textTransform='uppercase'>
-            Title
-          </Heading>
-          <Text pt='2' fontSize='sm'>
-            {title}
-          </Text>
-        </Box>
+        <CardBody>
+          <Stack divider={<StackDivider />} spacing='4'>
+            <Box>
+              <Heading size='xs' textTransform='uppercase'>
+                Category
+              </Heading>
+              <Text pt='2' fontSize='sm'>
+                {category}
+              </Text>
+            </Box>
+            
+            <Box>
+              <Heading size='xs' textTransform='uppercase'>
+                Title
+              </Heading>
+              <Text pt='2' fontSize='sm'>
+                {title}
+              </Text>
+            </Box>
 
-        <Box>
-          <Heading size='xs' textTransform='uppercase'>
-            Description
-          </Heading>
-          <Text pt='2' fontSize='sm'>
-            {description}
-          </Text>
-        </Box>
+            <Box>
+              <Heading size='xs' textTransform='uppercase'>
+                Description
+              </Heading>
+              <Text pt='2' fontSize='sm'>
+                {description}
+              </Text>
+            </Box>
 
-        <Box>
-          <Heading size='xs' textTransform='uppercase'>
-            Type of Ad
-          </Heading>
-          <Text pt='2' fontSize='sm'>
-            {is_sell_ad ? 'Sell Ad' : 'Buy Ad'}
-          </Text>
-        </Box>
+            <Box>
+              <Heading size='xs' textTransform='uppercase'>
+                Type of Ad
+              </Heading>
+              <Text pt='2' fontSize='sm'>
+                {is_sell_ad ? 'Sell Ad' : 'Buy Ad'}
+              </Text>
+            </Box>
 
-        <Box>
-          <Heading size='xs' textTransform='uppercase'>
-            Price
-          </Heading>
-          <Text pt='2' fontSize='sm'>
-            {price}
-          </Text>
-        </Box>
+            <Box>
+              <Heading size='xs' textTransform='uppercase'>
+                Price
+              </Heading>
+              <Text pt='2' fontSize='sm'>
+                {price}
+              </Text>
+            </Box>
 
-        <Box>
-          <Heading size='xs' textTransform='uppercase'>
-            Is Price Negotiable?
-          </Heading>
-          <Text pt='2' fontSize='sm'>
-            {is_negotiable ? 'Price is negotiable' : 'Price is not negotiable'}
-          </Text>
-        </Box>
+            <Box>
+              <Heading size='xs' textTransform='uppercase'>
+                Is Price Negotiable?
+              </Heading>
+              <Text pt='2' fontSize='sm'>
+                {is_negotiable ? 'Price is negotiable' : 'Price is not negotiable'}
+              </Text>
+            </Box>
 
-        <Box>
-          <Heading size='xs' textTransform='uppercase'>
-            Is Product Used?
-          </Heading>
-          <Text pt='2' fontSize='sm'>
-            {is_used ? 'Used Product' : 'New Product'}
-          </Text>
-        </Box>
+            <Box>
+              <Heading size='xs' textTransform='uppercase'>
+                Is Product Used?
+              </Heading>
+              <Text pt='2' fontSize='sm'>
+                {is_used ? 'Used Product' : 'New Product'}
+              </Text>
+            </Box>
 
-        <Box>
-          <Heading size='xs' textTransform='uppercase'>
-            Time of Use
-          </Heading>
-          <Text pt='2' fontSize='sm'>
-          {!is_used ? "N/A for new products" : (
-            <>
-              {years_used !== undefined && <span>{years_used} years</span>}
-              {months_used !== undefined && <span>{years_used ? ", " : ""}{months_used} months</span>}
-              {days_used !== undefined && <span>{(years_used || months_used) ? ", " : ""}{days_used} days</span>}
-            </>
-          )}
-          </Text>
-        </Box>
+            <Box>
+              <Heading size='xs' textTransform='uppercase'>
+                Time of Use
+              </Heading>
+              <Text pt='2' fontSize='sm'>
+              {!is_used ? "N/A for new products" : (
+                <>
+                  {years_used !== undefined && <span>{years_used} years</span>}
+                  {months_used !== undefined && <span>{years_used ? ", " : ""}{months_used} months</span>}
+                  {days_used !== undefined && <span>{(years_used || months_used) ? ", " : ""}{days_used} days</span>}
+                </>
+              )}
+              </Text>
+            </Box>
 
-        <Box>
-          <Heading size='xs' textTransform='uppercase'>
-            Images
-          </Heading>
-          <Text pt='2' fontSize='sm'>
-            {images && images.length > 0 ? (
-              images.map((image, index) => (
-                <span key={index}>{image}</span>
-              ))
-            ) : (
-              "No images provided"
-            )}
-          </Text>
-        </Box>
+            <Box>
+              <Heading size='xs' textTransform='uppercase'>
+                Images
+              </Heading>
+              <Text pt='2' fontSize='sm'>
+                {images && images.length > 0 ? (
+                  images.map((image, index) => (
+                    <span key={index}>{image}</span>
+                  ))
+                ) : (
+                  "No images provided"
+                )}
+              </Text>
+            </Box>
 
-        <Box>
-          <Heading size='xs' textTransform='uppercase'>
-            Is Phone Number Public?
-          </Heading>
-          <Text pt='2' fontSize='sm'>
-            {is_phone_public ? 'Phone number is public' : 'Phone number is private'}
-          </Text>
-        </Box>
+            <Box>
+              <Heading size='xs' textTransform='uppercase'>
+                Is Phone Number Public?
+              </Heading>
+              <Text pt='2' fontSize='sm'>
+                {is_phone_public ? 'Phone number is public' : 'Phone number is private'}
+              </Text>
+            </Box>
 
-        <Box>
-          <Heading size='xs' textTransform='uppercase'>
-            Address
-          </Heading>
-          <Text pt='2' fontSize='sm'>
-            {address}
-          </Text>
-        </Box>
+            <Box>
+              <Heading size='xs' textTransform='uppercase'>
+                Address
+              </Heading>
+              <Text pt='2' fontSize='sm'>
+                {address}
+              </Text>
+            </Box>
 
-      </Stack>
-    </CardBody>
-  </Card>
+          </Stack>
+        </CardBody>
+      </Card>
 
-  
+      <Flex justifyContent={"space-between"}>
+        <Button
+          onClick={onPrev}
+          bg={"teal.400"}
+          color={"white"}
+          _hover={{
+            bg: "blue.500",
+          }}
+        >
+          Previous
+        </Button>
+        <Button
+          // isDisabled={isCurrentInputInValid()}
+          onClick={onSubmit}
+          bg={"teal.400"}
+          color={"white"}
+          _hover={{
+            bg: "blue.500",
+          }}
+        >
+          Submit
+        </Button>
+      </Flex>
+    </>
   )
 }
 

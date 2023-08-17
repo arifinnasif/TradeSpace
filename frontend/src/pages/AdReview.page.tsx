8093f2a89895c3879b003d4bdd55c120d65d4292
Sultanul Arifin <msa336@outlook.com>
2023-08-18 00:20:44 +0600

    review page reformatted from get ads page

--- frontend/src/pages/AdReview.page.tsx
+++ frontend/src/pages/AdReview.page.tsx
@@ -1,21 +1,27 @@
 import { useState } from "react";
 import AdminLayout from "../layout/AdminLayout";
-import { Grid, GridItem } from "@chakra-ui/react";
+import { Center, Grid, GridItem } from "@chakra-ui/react";
+import ReviewList from "../components/Admin/ReviewList";
 
 const AdReviewPage = () => {
   const [isLoading, setIsLoading] = useState(false);
   return (
     <AdminLayout title="Ad Reviews" loading={isLoading}>
-      <Grid
-        templateAreas={`"filter_section ad_section"`}
+      {/* <Grid
+        templateAreas={`"filter_section review_section"`}
         gridTemplateColumns={"1fr 700px"}
         color={"teal.600"}
         mx={"70px"}
         my={"10px"}
       >
         <GridItem area={"filter_section"}></GridItem>
-        <GridItem area={"ad_section"}></GridItem>
-      </Grid>
+        <GridItem area={"review_section"}>
+          <ReviewList />
+        </GridItem>
+      </Grid> */}
+      <Center>
+        <ReviewList />
+      </Center>
     </AdminLayout>
   );
 };

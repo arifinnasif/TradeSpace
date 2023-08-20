342c0c4879ec7a82b8f44e69cb00e10fd03459a7
Sultanul Arifin <msa336@outlook.com>
2023-08-20 22:22:14 +0600

    dummy paginator styled

--- frontend/src/pages/AdReview.page.tsx
+++ frontend/src/pages/AdReview.page.tsx
@@ -1,8 +1,20 @@
 import { useEffect, useState } from "react";
 import AdminLayout from "../layout/AdminLayout";
-import { Center, Text, useColorModeValue } from "@chakra-ui/react";
+import {
+  Button,
+  Center,
+  HStack,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Text,
+  useColorModeValue,
+} from "@chakra-ui/react";
 import ReviewList from "../components/Admin/ReviewList";
 import { ReviewCardType, getAdReviews } from "../services/admin.service";
+import { ArrowBackIcon, ArrowForwardIcon } from "@chakra-ui/icons";
 
 const AdReviewPage = () => {
   const [isLoading, setIsLoading] = useState(false);
@@ -32,6 +44,74 @@ const AdReviewPage = () => {
 
   const textColor = useColorModeValue("gray.600", "gray.300");
 
+  const [currentPage, setCurrentPage] = useState(1);
+  const maxPageToShow = 5;
+  let maxPage = 10;
+
+  const startPageToShow = () => {
+    if (currentPage <= Math.ceil(maxPageToShow / 2)) {
+      return 1;
+    }
+    if (currentPage + Math.floor(maxPageToShow / 2) >= maxPage) {
+      return maxPage - maxPageToShow + 1;
+    }
+    return currentPage - Math.floor(maxPageToShow / 2);
+  };
+
+  const endPageToShow = () => {
+    return Math.min(startPageToShow() + maxPageToShow - 1, maxPage);
+  };
+
+  const currentPageIndex = () => {
+    return currentPage - startPageToShow();
+  };
+
+  const isPrevEnabled = () => {
+    if (currentPage <= 1) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  const isNextEnabled = () => {
+    if (currentPage >= maxPage) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  const prevButtonAction = () => {
+    if (!isPrevEnabled()) {
+      return;
+    }
+    setCurrentPage(currentPage - 1);
+  };
+
+  const nextButtonAction = () => {
+    if (!isNextEnabled()) {
+      return;
+    }
+    setCurrentPage(currentPage + 1);
+  };
+
+  const pageButtonAction = (arg_page: number) => {
+    setCurrentPage(arg_page);
+  };
+
+  const pageButtons = () => {
+    const buttons = [];
+    for (let i = startPageToShow(); i <= endPageToShow(); i++) {
+      buttons.push(
+        <Tab key={i} onClick={() => pageButtonAction(i)}>
+          {i}
+        </Tab>
+      );
+    }
+    return buttons;
+  };
+
   return (
     <AdminLayout title="Ad Reviews" loading={isLoading}>
       {/* <Grid
@@ -55,6 +135,22 @@ const AdReviewPage = () => {
           </Text>
         )}
       </Center>
+      <HStack>
+        <Button isDisabled={!isPrevEnabled()} onClick={prevButtonAction}>
+          <ArrowBackIcon />
+        </Button>
+        <Tabs
+          variant="soft-rounded"
+          colorScheme="teal"
+          defaultIndex={0}
+          index={currentPageIndex()}
+        >
+          <TabList>{pageButtons()}</TabList>
+        </Tabs>
+        <Button isDisabled={!isNextEnabled()} onClick={nextButtonAction}>
+          <ArrowForwardIcon />
+        </Button>
+      </HStack>
     </AdminLayout>
   );
 };

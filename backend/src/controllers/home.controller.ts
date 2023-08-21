24870d3fac6047bcb002def512d07cd366b0a692
ansakib <1705107@ugrad.cse.buet.ac.bd>
2023-08-21 12:28:38 +0600

    category list

--- backend/src/controllers/home.controller.ts
+++ backend/src/controllers/home.controller.ts
@@ -23,4 +23,17 @@ let get_categories = async (req: Request, res: Response) => {
   }
 };
 
+let get_categories_with_ads_count = async (req: Request, res: Response) => {
+  //get the category list first
+  try {
+    const categories = await prisma.category.findMany({
+      select: {
+        name: true,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ error: "categories not found" });
+  }
+};
+
 export { get_categories };

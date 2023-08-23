7b9b2f0e8996562af70968a68fe37b2510e890a0
ansakib <1705107@ugrad.cse.buet.ac.bd>
2023-08-23 20:00:19 +0600

    Filter UI

--- frontend/src/components/Ads/Filter.tsx
+++ frontend/src/components/Ads/Filter.tsx
@@ -2,12 +2,19 @@ import React, { useState } from "react";
 import {
   ChakraProvider,
   Box,
+  Flex,
+  FormControl,
+  FormLabel,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
   Checkbox,
-  Select,
+  Tag,
+  TagLabel,
+  TagCloseButton,
   RadioGroup,
   Radio,
-  Stack,
-  Button,
 } from "@chakra-ui/react";
 
 const products = [
@@ -28,9 +35,19 @@ const products = [
   // ... other products
 ];
 
-const Filter = () => {
-  const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
+const App = () => {
+  const [selectedCategories, setSelectedCategories] = useState([]);
   const [sortOption, setSortOption] = useState("price-asc");
+
+  const handleCategoryChange = (category) => {
+    if (selectedCategories.includes(category)) {
+      setSelectedCategories((prevCategories) =>
+        prevCategories.filter((cat) => cat !== category)
+      );
+    } else {
+      setSelectedCategories((prevCategories) => [...prevCategories, category]);
+    }
+  };
 
   const filteredAndSortedProducts = products
     .filter(
@@ -45,57 +62,94 @@ const Filter = () => {
       return sortOrder === "asc" ? aValue - bValue : bValue - aValue;
     });
 
-  const handleCategoryChange = (category: string) => {
-    if (selectedCategories.includes(category)) {
-      setSelectedCategories((prevCategories) =>
-        prevCategories.filter((cat) => cat !== category)
-      );
-    } else {
-      setSelectedCategories((prevCategories) => [...prevCategories, category]);
-    }
-  };
-
   return (
     <ChakraProvider>
       <Box p={4}>
-        <Stack direction="row" spacing={4} align="center">
-          <Checkbox
-            isChecked={selectedCategories.length === 0}
-            onChange={() => setSelectedCategories([])}
-          >
-            All Categories
-          </Checkbox>
-          {["Category 1", "Category 2", "Category 3"].map((category) => (
-            <Checkbox
-              key={category}
-              isChecked={selectedCategories.includes(category)}
-              onChange={() => handleCategoryChange(category)}
-            >
-              {category}
-            </Checkbox>
-          ))}
-        </Stack>
-        <RadioGroup mt={4} onChange={setSortOption} value={sortOption}>
-          <Stack direction="row">
-            <Radio value="price-asc">Price Ascending</Radio>
-            <Radio value="price-desc">Price Descending</Radio>
-            <Radio value="days_used-asc">Days Used Ascending</Radio>
-            <Radio value="days_used-desc">Days Used Descending</Radio>
-          </Stack>
-        </RadioGroup>
-        <Stack mt={4}>
+        <Flex justify="space-between" align="center">
+          <FormControl>
+            <FormLabel>Filter by Categories</FormLabel>
+            <Menu>
+              <MenuButton as={Tag} cursor="pointer">
+                Select Categories
+              </MenuButton>
+              <MenuList>
+                {["Category 1", "Category 2", "Category 3"].map((category) => (
+                  <MenuItem key={category}>
+                    <Checkbox
+                      isChecked={selectedCategories.includes(category)}
+                      onChange={() => handleCategoryChange(category)}
+                    >
+                      {category}
+                    </Checkbox>
+                  </MenuItem>
+                ))}
+              </MenuList>
+            </Menu>
+            {selectedCategories.length > 0 && (
+              <Flex mt={2} flexWrap="wrap">
+                {selectedCategories.map((category) => (
+                  <Tag
+                    key={category}
+                    size="md"
+                    variant="solid"
+                    colorScheme="teal"
+                    m="1"
+                  >
+                    <TagLabel>{category}</TagLabel>
+                    <TagCloseButton
+                      onClick={() => handleCategoryChange(category)}
+                    />
+                  </Tag>
+                ))}
+              </Flex>
+            )}
+          </FormControl>
+          <FormControl>
+            <FormLabel>Sort by</FormLabel>
+            <Menu>
+              <MenuButton as={Tag} cursor="pointer">
+                Sort by:{" "}
+                {sortOption === "price-asc"
+                  ? "Price Ascending"
+                  : "Price Descending"}
+              </MenuButton>
+              <MenuList>
+                <MenuItem onClick={() => setSortOption("price-asc")}>
+                  Price Ascending
+                </MenuItem>
+                <MenuItem onClick={() => setSortOption("price-desc")}>
+                  Price Descending
+                </MenuItem>
+                <MenuItem onClick={() => setSortOption("days_used-asc")}>
+                  Days Used Ascending
+                </MenuItem>
+                <MenuItem onClick={() => setSortOption("days_used-desc")}>
+                  Days Used Descending
+                </MenuItem>
+              </MenuList>
+            </Menu>
+          </FormControl>
+        </Flex>
+        <Box mt={4}>
           {filteredAndSortedProducts.map((product) => (
-            <Box key={product.id} borderWidth="1px" p={2}>
+            <Box
+              key={product.id}
+              borderWidth="1px"
+              borderRadius="lg"
+              p={4}
+              shadow="md"
+              my={2}
+            >
               <strong>{product.name}</strong>
               <div>Category: {product.category}</div>
               <div>Price: {product.price}</div>
               <div>Days Used: {product.days_used}</div>
             </Box>
           ))}
-        </Stack>
+        </Box>
       </Box>
     </ChakraProvider>
   );
 };
 
-export default Filter;
+export default App;

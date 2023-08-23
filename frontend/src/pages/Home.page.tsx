36819679c600231e3be853bc20eb0dfc967859a1
ansakib <1705107@ugrad.cse.buet.ac.bd>
2023-08-23 17:17:06 +0600

    homepage searchbar

--- frontend/src/pages/Home.page.tsx
+++ frontend/src/pages/Home.page.tsx
@@ -3,9 +3,30 @@ import Layout from "../layout/Layout";
 import CategoryList from "../components/Homepage/CategoryList";
 import SearchComponent from "../components/Homepage/SearchBar";
 import { Grid, GridItem } from "@chakra-ui/layout";
+import {
+  useSearchParams,
+  useNavigate,
+  createSearchParams,
+} from "react-router-dom";
 
 const HomePage = () => {
   const [isLoading, setIsLoading] = useState(false);
+  const [params, setParams] = useSearchParams();
+  const navigate = useNavigate();
+
+  const handleSearchSubmit = (search_term: string) => {
+    const search_param = {
+      search_string: search_term,
+    };
+    // console.log(search_param);
+    setParams(search_param);
+    navigate({
+      pathname: "/ads",
+      search: `?${createSearchParams(search_param)}`,
+    });
+
+    navigate(0);
+  };
   return (
     <Layout title="Home" loading={isLoading}>
       <Grid
@@ -16,7 +37,7 @@ const HomePage = () => {
         m={"10"}
       >
         <GridItem bgColor={""} area={"SearchBar_section"}>
-          <SearchComponent />
+          <SearchComponent onEnterKey={handleSearchSubmit} />
         </GridItem>
         <GridItem bgColor={""} area={"CategoryList_section"}>
           <CategoryList />

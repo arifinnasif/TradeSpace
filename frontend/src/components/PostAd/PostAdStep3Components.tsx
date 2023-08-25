ee4293b6e2d012ca290d9cba4f881cdbefcc60c3
Sheam1685 <samia12676@gmail.com>
2023-08-25 14:26:17 +0600

    popover content to show the map introduced

--- frontend/src/components/PostAd/PostAdStep3Components.tsx
+++ frontend/src/components/PostAd/PostAdStep3Components.tsx
@@ -12,8 +12,25 @@ import {
   Box,
   Flex,
   useToast,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverArrow,
+  PopoverBody,
+  PopoverCloseButton,
+  PopoverHeader,
 } from "@chakra-ui/react";
-import { FunctionComponent, useEffect, useState } from "react";
+
+import { FunctionComponent, 
+         useEffect, 
+         useState 
+} from "react";
+
+import { MapContainer, 
+         TileLayer, 
+         Marker, 
+         Popup 
+} from "react-leaflet";
 
 
 
@@ -166,6 +183,42 @@ const Step3: FunctionComponent<Step3Props> = ({
       <FormControl isRequired isInvalid={addressError} onBlur={handleAddressTouched}>
         <FormLabel>Address</FormLabel>
         <Input type="text" placeholder="Address" value={address} onChange={handleAddressChange} />
+
+        {/* map */}
+        <br/>
+        <br/>
+
+        <Popover placement="bottom" closeOnBlur={false}> 
+          <PopoverTrigger>
+            <Button>
+              Show Map
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent>
+            <PopoverArrow />
+            <PopoverCloseButton />
+            <PopoverHeader>Map</PopoverHeader>
+            <PopoverBody>
+            <div style={{ width: '100%', height: '100%', boxSizing: 'border-box' }}>
+              <MapContainer
+                center={[51.505, -0.09]}
+                zoom={13}
+                style={{ width: '100%', height: '100%' }}
+              >
+                <TileLayer
+                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                />
+                <Marker position={[51.505, -0.09]}>
+                  <Popup>
+                    A pretty CSS3 popup. <br /> Easily customizable.
+                  </Popup>
+                </Marker>
+              </MapContainer>
+            </div>
+            </PopoverBody>
+          </PopoverContent>
+        </Popover>
         {addressError && 
         <FormErrorMessage>Address should be at the range of 5-50 characters</FormErrorMessage>}
       </FormControl>

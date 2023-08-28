a781781883a3666fab6bf0d41ff7e0b8c535dde5
Sultanul Arifin <msa336@outlook.com>
2023-08-28 12:22:20 +0600

    image retrival and imbedding successful

--- frontend/src/pages/Test.page.tsx
+++ frontend/src/pages/Test.page.tsx
@@ -1,10 +1,11 @@
 // just a header to show that the page is under construction
 
-import { Text, Center } from "@chakra-ui/react";
+import { Text, Center, Image } from "@chakra-ui/react";
 import { ChangeEvent, useState } from "react";
 
 // Import the functions you need from the SDKs you need
-import { storage } from "../firebase";
+import { uploadFile } from "../services/fileupload.service";
+
 import {
   UploadResult,
   getDownloadURL,
@@ -14,17 +15,14 @@ import {
 
 const Test = () => {
   const [file, setFile] = useState<File>();
-  const [uploadingFiles, setUploadingFiles] = useState([]);
+  const [uploadedFilesURLList, setuploadedFilesURLList] = useState<string[]>(
+    []
+  );
 
   const clickAction = async () => {
-    console.log(file);
     if (file == null) return;
-    const imageRef = ref(storage, `images/${file.name}`);
-    uploadBytes(imageRef, file).then((snapshot: UploadResult) => {
-      getDownloadURL(snapshot.ref).then((url: string) => {
-        // setImageUrls((prev) => [...prev, url]);
-        console.log(url);
-      });
+    await uploadFile(file, "ads").then((url: string) => {
+      setuploadedFilesURLList((prev) => [...prev, url]);
     });
   };
 
@@ -35,11 +33,18 @@ const Test = () => {
   };
 
   return (
-    <Center>
-      <input type="file" name="file" onChange={handleFileChange} />
-      <div>{file && `${file.name} - ${file.type}`}</div>
-      <button onClick={clickAction}>upload</button>
-    </Center>
+    <>
+      <Center>
+        <input type="file" name="file" onChange={handleFileChange} />
+        <div>{file && `${file.name} - ${file.type}`}</div>
+        <button onClick={clickAction}>upload</button>
+      </Center>
+      <Center>
+        {uploadedFilesURLList.map((url, key) => (
+          <Image key={key} src={url} />
+        ))}
+      </Center>
+    </>
   );
 };
 

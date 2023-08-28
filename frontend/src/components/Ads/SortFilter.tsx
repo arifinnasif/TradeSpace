3719030ae473b3233e832a22775ae49e06321762
ansakib <1705107@ugrad.cse.buet.ac.bd>
2023-08-28 13:27:11 +0600

    callback function of sortFIlter

--- frontend/src/components/Ads/SortFilter.tsx
+++ frontend/src/components/Ads/SortFilter.tsx
@@ -13,47 +13,74 @@ import {
   TagLabel,
   FormControl,
 } from "@chakra-ui/react";
+import { FunctionComponent, useState } from "react";
 
-const sortFilter = (props: any) => {
-  <FormControl>
-    <FormLabel>Sort by</FormLabel>
-    <Menu>
-      <MenuButton as={Button}>Sort Options</MenuButton>
-      <MenuList>
-        <MenuItem>
-          <FormLabel>Sort Field</FormLabel>
-          <RadioGroup value={sortField} onChange={handleSortFieldChange}>
-            <Stack spacing={2}>
-              <Radio value="price">Price</Radio>
-              <Radio value="days_used">Days Used</Radio>
-            </Stack>
-          </RadioGroup>
-        </MenuItem>
-        <MenuItem>
-          <FormLabel>Sort Order</FormLabel>
-          <RadioGroup value={sortOrder} onChange={handleSortOrderChange}>
-            <Stack spacing={2}>
-              <Radio value="asc">Ascending</Radio>
-              <Radio value="desc">Descending</Radio>
-            </Stack>
-          </RadioGroup>
-        </MenuItem>
-      </MenuList>
-    </Menu>
-    <Flex mt={2}>
-      {sortField && sortOrder && (
-        <>
-          <Tag m="1" size="md" variant="solid" colorScheme="blue">
-            {/* if sortField==price, tagLabel is Price */}
-            <TagLabel>{sortField === "price" ? "Price" : "Days Used"}</TagLabel>
-          </Tag>
-          <Tag m="1" size="md" variant="solid" colorScheme="blue">
-            <TagLabel>
-              {sortOrder === "asc" ? "Ascending" : "Descending"}
-            </TagLabel>
-          </Tag>
-        </>
-      )}
-    </Flex>
-  </FormControl>;
+interface SortFilterProps {
+  handleSortFieldChange: (field: React.SetStateAction<string>) => void;
+  handleSortOrderChange: (order: React.SetStateAction<string>) => void;
+}
+
+const SortFilter: FunctionComponent<SortFilterProps> = ({
+  handleSortFieldChange,
+  handleSortOrderChange,
+}) => {
+  const [sortField, setSortField] = useState("price");
+  const [sortOrder, setSortOrder] = useState("desc");
+
+  const callSortFieldChange = (field: React.SetStateAction<string>) => {
+    setSortField(field);
+    handleSortFieldChange(field);
+  };
+
+  const callSortOrderChange = (order: React.SetStateAction<string>) => {
+    setSortOrder(order);
+    handleSortOrderChange(order);
+  };
+  return (
+    <FormControl>
+      <FormLabel>Sort by</FormLabel>
+      <Menu>
+        <MenuButton as={Button}>Sort Options</MenuButton>
+        <MenuList>
+          <MenuItem>
+            <FormLabel>Sort Field</FormLabel>
+            <RadioGroup value={sortField} onChange={callSortFieldChange}>
+              <Stack spacing={2}>
+                <Radio value="price">Price</Radio>
+                <Radio value="days_used">Days Used</Radio>
+              </Stack>
+            </RadioGroup>
+          </MenuItem>
+          <MenuItem>
+            <FormLabel>Sort Order</FormLabel>
+            <RadioGroup value={sortOrder} onChange={callSortOrderChange}>
+              <Stack spacing={2}>
+                <Radio value="asc">Ascending</Radio>
+                <Radio value="desc">Descending</Radio>
+              </Stack>
+            </RadioGroup>
+          </MenuItem>
+        </MenuList>
+      </Menu>
+      <Flex mt={2}>
+        {sortField && sortOrder && (
+          <>
+            <Tag m="1" size="md" variant="solid" colorScheme="blue">
+              {/* if sortField==price, tagLabel is Price */}
+              <TagLabel>
+                {sortField === "price" ? "Price" : "Days Used"}
+              </TagLabel>
+            </Tag>
+            <Tag m="1" size="md" variant="solid" colorScheme="blue">
+              <TagLabel>
+                {sortOrder === "asc" ? "Ascending" : "Descending"}
+              </TagLabel>
+            </Tag>
+          </>
+        )}
+      </Flex>
+    </FormControl>
+  );
 };
+
+export default SortFilter;

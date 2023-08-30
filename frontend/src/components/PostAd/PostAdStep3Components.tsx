e9237e28ddd74e039a1bbcd20fa0a050b2e77455
Sultanul Arifin <msa336@outlook.com>
2023-08-30 16:20:06 +0600

    issues after merge resolved

--- frontend/src/components/PostAd/PostAdStep3Components.tsx
+++ frontend/src/components/PostAd/PostAdStep3Components.tsx
@@ -3,10 +3,10 @@
 import { AddIcon } from "@chakra-ui/icons";
 
 import {
-  Image,
   FormControl,
   FormLabel,
   Input,
+  Image,
   Button,
   FormHelperText,
   Checkbox,
@@ -23,23 +23,18 @@ import {
   PopoverCloseButton,
   HStack,
 } from "@chakra-ui/react";
+
 import { FunctionComponent, useEffect, useState } from "react";
-import { uploadFile } from "../../services/fileupload.service";
 
-  
-
-import { FunctionComponent, 
-         useEffect,  
-         useState 
-} from "react";
-
-import { MapContainer, 
-         TileLayer, 
-         Marker, 
-         Popup, 
-         useMapEvents
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMapEvents,
 } from "react-leaflet";
 
+import { uploadFile } from "../../services/fileupload.service";
 
 import { ReactSearchAutocomplete } from "react-search-autocomplete";
 
@@ -48,51 +43,46 @@ import { ReactSearchAutocomplete } from "react-search-autocomplete";
 // without it, the map will show up with different tiles popping up
 // at different positions like there's no tomorrow
 // Also a height is required for the map to show up
-// I set the height at 500px in the PopoverContent 
-import "leaflet/dist/leaflet.css"
+// I set the height at 500px in the PopoverContent
+import "leaflet/dist/leaflet.css";
 import { LatLng, marker } from "leaflet";
 
-
 // This function is necessary for the map to show current location
-function LocationMarker({ markerPosition,
-                          setMarkerPosition,
-                          setMapCenter
-                        } : { markerPosition : LatLng | null,
-                              setMarkerPosition : (position : LatLng) => void,
-                              setMapCenter : (position : LatLng) => void
-                            }) 
-{
- 
+function LocationMarker({
+  markerPosition,
+  setMarkerPosition,
+  setMapCenter,
+}: {
+  markerPosition: LatLng | null;
+  setMarkerPosition: (position: LatLng) => void;
+  setMapCenter: (position: LatLng) => void;
+}) {
   const map = useMapEvents({
     locationfound(e) {
-      setMapCenter(e.latlng)
-      setMarkerPosition(e.latlng)
-      map.flyTo(e.latlng, map.getZoom())
+      setMapCenter(e.latlng);
+      setMarkerPosition(e.latlng);
+      map.flyTo(e.latlng, map.getZoom());
     },
-  })
- 
+  });
+
   useEffect(() => {
-    if (markerPosition == null)
-      map.locate();
+    if (markerPosition == null) map.locate();
   }, []);
 
   return markerPosition === null ? null : (
-    <Marker position={markerPosition}
-            draggable={true}
-            eventHandlers={{
-              dragend: (e) => {
-                setMarkerPosition(e.target.getLatLng());
-              },
-            }}
+    <Marker
+      position={markerPosition}
+      draggable={true}
+      eventHandlers={{
+        dragend: (e) => {
+          setMarkerPosition(e.target.getLatLng());
+        },
+      }}
     >
       <Popup>Move the marker to show your address</Popup>
     </Marker>
-  )
+  );
 }
-
-
-
-
 
 interface Step3Props {
   onPrev: () => void;
@@ -123,7 +113,6 @@ const Step3: FunctionComponent<Step3Props> = ({
   setMarkerPosition,
   setMapCenter,
 }) => {
-
   // selectedFile
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
   const [isBusyUploading, setIsBusyUploading] = useState<boolean>(false);
@@ -140,18 +129,12 @@ const Step3: FunctionComponent<Step3Props> = ({
       console.log("done");
     });
   };
-
-  // declaration of error states
-  const [addressError, setAddressError] = useState<boolean>(false);
-
-
   // Map related states
   // const position = { lat: 51.5704, lng: 0.1276 }
   const initialPosition = new LatLng(51.5704, 0.1276);
   // const [position, setPosition] = useState<LatLng>(initialPosition);
   const [showMap, setShowMap] = useState(false);
   const [mapKey, setMapKey] = useState(0);
-
 
   // map related functions
   const handleShowMap = () => {
@@ -160,24 +143,15 @@ const Step3: FunctionComponent<Step3Props> = ({
     setMapKey(Math.random());
   };
 
-
-
-
-
-
-
   // declaration of error states
-  const [addressError, setAddressError] = useState<boolean>(false)
-  const [mapError, setMapError] = useState<boolean>(false)
-
-
+  const [addressError, setAddressError] = useState<boolean>(false);
+  const [mapError, setMapError] = useState<boolean>(false);
 
   // declaration of touched states and action
   const [addressTouched, setAddressTouched] = useState<boolean>(false);
   const handleAddressTouched = () => {
     setAddressTouched(true);
   };
-
 
   // change events definition
   const handleImagesChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -200,7 +174,6 @@ const Step3: FunctionComponent<Step3Props> = ({
 
   // error checking hooks for constant supervision
   useEffect(() => {
-
     if (
       addressTouched &&
       (address == undefined || address.length < 5 || address.length > 50)
@@ -211,48 +184,28 @@ const Step3: FunctionComponent<Step3Props> = ({
     }
   }, [address, addressTouched]);
 
+  useEffect(() => {
+    if (
+      addressTouched &&
+      (markerPosition == undefined || markerPosition == null)
+    ) {
+      setMapError(true);
+    } else {
+      setMapError(false);
+    }
+  }, [markerPosition, addressTouched]);
+
   // validation function
   const isUserInputValid = () => {
     if (
       addressError ||
+      // mapError ||
       address == undefined ||
       address.length < 5 ||
-      address.length > 50
+      address.length > 50 ||
+      markerPosition == undefined ||
+      markerPosition == null
     )
-
-    if (addressTouched && (address == undefined || address.length < 5 || address.length > 50)) {
-      setAddressError(true)
-    }
-    else {
-      setAddressError(false)
-    }
-    
-  }, [address, addressTouched])
-
-
-  useEffect(() => {
-    if(addressTouched && (markerPosition == undefined || markerPosition == null)) {
-      setMapError(true)
-    } 
-    else {
-      setMapError(false)
-    }
-  }, [markerPosition, addressTouched])
-
-
-
-
-  // validation function
-  const isUserInputValid = () => {
-    if (addressError || 
-        // mapError ||
-        address == undefined || 
-        address.length < 5 || 
-        address.length > 50 ||
-        markerPosition == undefined ||
-        markerPosition == null
-       ) 
-
       return false;
 
     return true;
@@ -260,35 +213,26 @@ const Step3: FunctionComponent<Step3Props> = ({
 
   const toast = useToast();
 
-
   const handleNextRequest = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
     if (isUserInputValid()) {
       console.log(is_phone_public);
       console.log(address);
+      console.log(markerPosition);
       console.log(images);
-
-
-  const handleNextRequest = (e : React.MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault()
-    if(isUserInputValid()){
-      console.log(is_phone_public)
-      console.log(address)
-      console.log(markerPosition)
-      console.log(images)
       onNext();
     } else {
       console.log("Invalid Inputs");
       toast({
         title: "Invalid Inputs",
-        description: "Please check your inputs and try again.\nMake sure to place your address in the map",
+        description:
+          "Please check your inputs and try again.\nMake sure to place your address in the map",
         status: "error",
         duration: 5000,
         isClosable: true,
       });
     }
   };
-
 
   const countries = [
     "nigeria",
@@ -297,7 +241,6 @@ const Step3: FunctionComponent<Step3Props> = ({
     "united states",
     "south korea",
   ];
-
 
   return (
     <>
@@ -349,21 +292,33 @@ const Step3: FunctionComponent<Step3Props> = ({
         </Checkbox>
       </FormControl>
 
-
-      <FormControl isRequired isInvalid={addressError} onBlur={handleAddressTouched}>
+      <FormControl
+        isRequired
+        isInvalid={addressError}
+        onBlur={handleAddressTouched}
+      >
         <FormLabel>Describe your Address</FormLabel>
-        <Input type="text" placeholder="Address" value={address} onChange={handleAddressChange} />
-        {addressError && 
-        <FormErrorMessage>Address description should be at the range of 5-50 characters</FormErrorMessage>}
+        <Input
+          type="text"
+          placeholder="Address"
+          value={address}
+          onChange={handleAddressChange}
+        />
+        {addressError && (
+          <FormErrorMessage>
+            Address description should be at the range of 5-50 characters
+          </FormErrorMessage>
+        )}
 
-        <br/>
-        <br/>
+        <br />
+        <br />
 
-        <Popover placement="bottom" 
-                 closeOnBlur={false} 
-                 isOpen={showMap} 
-                //  key={mapKey}
-        > 
+        <Popover
+          placement="bottom"
+          closeOnBlur={false}
+          isOpen={showMap}
+          //  key={mapKey}
+        >
           <PopoverTrigger>
             {/* <Button onClick={handleShowMap}>
               Mark Location on Map
@@ -380,9 +335,11 @@ const Step3: FunctionComponent<Step3Props> = ({
               Mark Location on Map
             </Button>
           </PopoverTrigger>
-          <PopoverContent style={{ width: '500px', height: '500px', overflow: 'hidden'}}>
+          <PopoverContent
+            style={{ width: "500px", height: "500px", overflow: "hidden" }}
+          >
             <PopoverArrow />
-            <PopoverCloseButton onClick={() => setShowMap(false)}/>
+            <PopoverCloseButton onClick={() => setShowMap(false)} />
             <PopoverHeader>
               Choose your address on map
               {/* add a search box later if possible */}
@@ -396,33 +353,40 @@ const Step3: FunctionComponent<Step3Props> = ({
                 />
               </FormControl> */}
             </PopoverHeader>
-            <PopoverBody style={{ width: '100%', height: '100%', overflow: 'hidden'}}>
-            <div style={{ width: '100%', height: '100%', boxSizing: 'border-box' }}>
-               
-                           
-              <MapContainer
-                center={mapCenter}
-                zoom={17}
-                scrollWheelZoom={false}
-                style={{ width: '100%', height: '100%' }}
+            <PopoverBody
+              style={{ width: "100%", height: "100%", overflow: "hidden" }}
+            >
+              <div
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  boxSizing: "border-box",
+                }}
               >
-                <TileLayer
-                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                />
-                {/* <Marker position={position}>
+                <MapContainer
+                  center={mapCenter}
+                  zoom={17}
+                  scrollWheelZoom={false}
+                  style={{ width: "100%", height: "100%" }}
+                >
+                  <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                  />
+                  {/* <Marker position={position}>
                   <Popup>
                     A pretty CSS3 popup. <br /> Easily customizable.
                   </Popup>
                 </Marker> */}
-                <LocationMarker markerPosition={markerPosition}
-                                setMapCenter={setMapCenter}
-                                setMarkerPosition={setMarkerPosition}
-                />
-              </MapContainer>
-            </div>
+                  <LocationMarker
+                    markerPosition={markerPosition}
+                    setMapCenter={setMapCenter}
+                    setMarkerPosition={setMarkerPosition}
+                  />
+                </MapContainer>
+              </div>
             </PopoverBody>
-            
+
             {markerPosition && (
               <div>
                 <p>Current Latitude: {markerPosition.lat.toFixed(6)}</p>
@@ -431,8 +395,11 @@ const Step3: FunctionComponent<Step3Props> = ({
             )}
           </PopoverContent>
         </Popover>
-        {mapError &&
-        <FormErrorMessage>Please place your address on the map</FormErrorMessage>}
+        {mapError && (
+          <FormErrorMessage>
+            Please place your address on the map
+          </FormErrorMessage>
+        )}
       </FormControl>
 
       <Flex justifyContent={"space-between"}>

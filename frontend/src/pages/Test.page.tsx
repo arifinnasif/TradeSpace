a75c768f226ff314132850cde4f4862638fa58bf
Sultanul Arifin <msa336@outlook.com>
2023-09-02 14:36:42 +0600

    ✨ added payment card

--- frontend/src/pages/Test.page.tsx
+++ frontend/src/pages/Test.page.tsx
@@ -12,14 +12,14 @@ import {
   Center,
 } from "@chakra-ui/react";
 // Here we have used react-icons package for the icons
-import { BiCheck } from "react-icons/bi";
+
 import { BsFillCloudCheckFill } from "react-icons/bs";
 import { AiOutlineCloudServer } from "react-icons/ai";
 import { FaServer } from "react-icons/fa";
-import { IconType } from "react-icons";
 import Layout from "../layout/Layout";
 import { useState } from "react";
 import AdCard from "../components/Promotions/AdCard";
+import PricingCard from "../components/Promotions/PricingCard";
 
 const plansList = [
   {
@@ -92,62 +92,6 @@ const ThreeTiersPricing = () => {
       </Container>
       {/* </Box> */}
     </Layout>
-  );
-};
-
-interface PricingCardProps {
-  title: string;
-  price: number;
-  features: string[];
-  icon: IconType;
-}
-
-const PricingCard = ({ title, price, icon, features }: PricingCardProps) => {
-  return (
-    <Box
-      minW={{ base: "xs", sm: "xs", lg: "sm" }}
-      rounded="md"
-      bg={useColorModeValue("teal.50", "teal.900")}
-      boxShadow="xl"
-      marginInline="auto"
-      my={3}
-      p={6}
-    >
-      <Box textAlign="center">
-        <Icon as={icon} h={10} w={10} color="teal.500" />
-        <chakra.h2 fontSize="2xl" fontWeight="bold">
-          {title}
-        </chakra.h2>
-        <Text fontSize="7xl" fontWeight="bold">
-          <Text as="sup" fontSize="3xl" fontWeight="normal" top="-1em">
-            ৳
-          </Text>
-          {price}
-        </Text>
-        <Text fontSize="md" color="gray.500">
-          per month
-        </Text>
-      </Box>
-      <VStack spacing={2} alignItems="flex-start" my={6}>
-        {features.map((feature, index) => (
-          <HStack key={index} spacing={3}>
-            <Icon as={BiCheck} h={4} w={4} color="green.500" />
-            <Text fontSize="sm" color="gray.500">
-              {feature}
-            </Text>
-          </HStack>
-        ))}
-      </VStack>
-      <Button
-        colorScheme="teal"
-        variant="solid"
-        size="md"
-        rounded="md"
-        w="100%"
-      >
-        Get Started
-      </Button>
-    </Box>
   );
 };
 

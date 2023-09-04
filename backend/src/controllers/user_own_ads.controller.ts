9ad6f5220a878dc1b2bea7488a3dba3d87446583
ansakib <1705107@ugrad.cse.buet.ac.bd>
2023-09-04 21:33:08 +0600

    get user ads controller

--- backend/src/controllers/user_own_ads.controller.ts
+++ backend/src/controllers/user_own_ads.controller.ts
@@ -4,6 +4,32 @@ import { Request, Response } from "express";
 import prisma from "../../prisma/prisma_client";
 
 // get user own ads: /api/profile/my_ads
-let get_user_own_ads = async (req: Request, res: Response) => {};
+let get_user_own_ads = async (req: Request, res: Response) => {
+  const user: any = req.user;
+
+  // retrieve user profile from db
+  try {
+    const userAds = await prisma.ads.findMany({
+      where: { op_username: user.username },
+      select: {
+        id: true,
+        title: true,
+        price: true,
+        category: true,
+        description: true,
+        images: true,
+        location: true,
+        status: true,
+        created_at: true,
+      },
+    });
+
+    // send user profile
+    res.status(200).json(userAds);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
 
 export { get_user_own_ads };

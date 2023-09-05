6f768af7fbd61bbd04ec788072958e15c9a05b09
Sheam1685 <samia12676@gmail.com>
2023-09-06 05:06:23 +0600

    notification seen status update works at backend

--- backend/src/controllers/user_notification.controller.ts
+++ backend/src/controllers/user_notification.controller.ts
@@ -1,11 +1,11 @@
-import prima from '../../prisma/prisma_client';
+import prisma from '../../prisma/prisma_client';
 
 
 
 // get all notifications: /api/profile/notifications
 export const get_user_notifications = async (req: any, res: any) => {
     try {
-        const notifications = await prima.notifications.findMany({
+        const notifications = await prisma.notifications.findMany({
             where: {
                 user: {
                     username: req.user.username
@@ -25,9 +25,48 @@ export const get_user_notifications = async (req: any, res: any) => {
     }
 }
 
+// update notification seen status: /api/profile/notifications
+export const update_notification_seen_status = async (req: any, res: any) => {
+    const user : any = req.user;
+
+    try {
+        // Find notifications for the specific user where is_seen is false
+        const notificationsToUpdate = await prisma.notifications.findMany({
+            where: {
+            username: user.username,
+            is_seen: false,
+            },
+        });
+
+
+        // Update the found notifications to set is_seen to true
+        const updatedNotifications = await prisma.notifications.updateMany({
+            where: {
+            id: {
+                in: notificationsToUpdate.map((notification) => notification.id),
+            },
+            },
+            data: {
+            is_seen: true,
+            },
+        });
+
+        return res.status(200).json({
+            success: true,
+            message: 'Notification seen status updated successfully!'
+        });
+    } catch (error: any) {
+        return res.status(500).json({
+            success: false,
+            error: error.message
+        });
+    }
+}
+    
+
 export const notify_user = async (username: string, type: string, title: string, description: string) => {
     try {
-        const user = await prima.users.findUnique({
+        const user = await prisma.users.findUnique({
             where: {
                 username: username
             }
@@ -35,7 +74,7 @@ export const notify_user = async (username: string, type: string, title: string,
 
         if (!user) return;
 
-        const notification = await prima.notifications.create({
+        const notification = await prisma.notifications.create({
             data: {
                 type: type,
                 title: title,

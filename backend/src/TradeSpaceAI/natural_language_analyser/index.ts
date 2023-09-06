99939bb14991f1a5eb42e56be053f3bdefa9aa44
Sultanul Arifin <msa336@outlook.com>
2023-09-06 17:01:54 +0600

    natural language analyzer verdict added

---
+++ backend/src/TradeSpaceAI/natural_language_analyser/index.ts
@@ -0,0 +1,15 @@
+import { detect_harmful_link } from './harmful_link_detector';
+import { detect_profanity } from './profanity_detector';
+
+export const exif_analyser = (text_to_check: string) => {
+
+
+    const nlp_verdict = {
+        has_banned_words: detect_profanity(text_to_check),
+        has_harmful_links: detect_harmful_link(text_to_check)
+    }
+
+
+    return nlp_verdict;
+
+}

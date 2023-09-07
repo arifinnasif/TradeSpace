f301cab57982f4d7bbb9469f69ec6133ed7689fe
Sheam1685 <samia12676@gmail.com>
2023-09-07 12:54:37 +0600

    sidebar and chatbox region separated

--- frontend/src/pages/Chats.page.tsx
+++ frontend/src/pages/Chats.page.tsx
@@ -1,11 +1,32 @@
+import { Flex, Grid, GridItem } from "@chakra-ui/react";
 
 
 
 const GetChats = () => {
     return(
-        <div>
-            <h1>Chats</h1>
-        </div>
+        <Grid height={"100vh"}
+              templateColumns="repeat(5, 1fr)" 
+              gap={4}
+        >
+            <GridItem colSpan={2}>
+                <Flex w="100%" h="100vh" justify="center" align="center">
+                    <Flex w={["100%", "100%", "40%"]} h="90%" flexDir="column">
+                        <div>
+                            <h1>Sidebar</h1>
+                        </div>
+                    </Flex>
+                </Flex>
+            </GridItem>
+            <GridItem colSpan={3}>
+                <Flex w="100%" h="100vh" justify="center" align="center">
+                    <Flex w={["100%", "100%", "40%"]} h="90%" flexDir="column">
+                        <div>
+                            <h1>Chats</h1>
+                        </div>
+                    </Flex>
+                </Flex>
+            </GridItem>
+        </Grid>
     )
 }
 

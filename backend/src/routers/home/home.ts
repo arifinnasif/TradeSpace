623e6254684072a24ad892108773cc3bebe8f196
ansakib <1705107@ugrad.cse.buet.ac.bd>
2023-09-08 02:09:15 +0600

    ads_by_cat API

--- backend/src/routers/home/home.ts
+++ backend/src/routers/home/home.ts
@@ -1,8 +1,13 @@
 import express from "express";
-import { get_categories_with_ads_count } from "../../controllers/home.controller";
+import {
+  get_ads_by_category,
+  get_categories_with_ads_count,
+} from "../../controllers/home.controller";
 
 const router = express.Router();
 
 router.route("/categories").get(get_categories_with_ads_count);
+
+router.route("/category/:category_name").get(get_ads_by_category);
 
 export default router;

aa17f404558c336ad72bf9c81d4c6fcfe32571fc
ansakib <1705107@ugrad.cse.buet.ac.bd>
2023-09-08 01:22:55 +0600

    declined_ads API

--- backend/src/controllers/user_profile.controller.ts
+++ backend/src/controllers/user_profile.controller.ts
@@ -209,9 +209,52 @@ let get_pending_ads = async (req: Request, res: Response) => {
   }
 };
 
+let get_declined_ads = async (req: Request, res: Response) => {
+  const user: any = req.user;
+
+  // retrieve Pending ads
+  try {
+    const declined_ads_list = await prisma.archived_ads.findMany({
+      where: { op_username: user.username },
+      select: {
+        id: true,
+        title: true,
+        description: true,
+        // category_name: true,
+        price: true,
+        // is_negotiable: true,
+        // is_used: true,
+        // is_sell_ad: true,
+        // days_used: true,
+        image1: true,
+        // image2: true,
+        // image3: true,
+        // image4: true,
+        // image5: true,
+        // receipt_image: true,
+        // is_phone_public: true,
+        address: true,
+        // latitude: true,
+        // longitude: true,
+        // promotion_type: true,
+        // status: true,
+        created_at: true,
+        reason: true,
+      },
+    });
+
+    // send result
+    res.status(200).json(declined_ads_list);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
 export {
   get_user_profile,
   update_user_profile,
   get_active_ads,
   get_pending_ads,
+  get_declined_ads,
 };

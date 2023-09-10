52b9fad75f9434ba346ee8aae36bcd37ae500e5d
Sultanul Arifin <msa336@outlook.com>
2023-09-10 08:19:59 +0600

    user table entry separated for using modal

---
+++ frontend/src/components/Admin/UserManagement/UserTableEntry.tsx
@@ -0,0 +1,44 @@
+import { Button, Td, Tr } from "@chakra-ui/react";
+import _ from "lodash";
+import { FunctionComponent } from "react";
+import { FaVolumeMute, FaVolumeUp } from "react-icons/fa";
+
+interface UserTableEntryProps {
+  username: string;
+  age: number;
+  gender: string;
+  created_at: string;
+  active_ads: number;
+  pending_ads: number;
+  is_muted: boolean;
+}
+
+const UserTableEntry: FunctionComponent<UserTableEntryProps> = ({
+  username,
+  age,
+  gender,
+  created_at,
+  active_ads,
+  pending_ads,
+  is_muted,
+}) => {
+  return (
+    <Tr>
+      <Td>{username}</Td>
+      <Td isNumeric>{age}</Td>
+      <Td>{_.startCase(gender)}</Td>
+      <Td>{new Date(created_at).toLocaleString()}</Td>
+      <Td isNumeric>{active_ads}</Td>
+      <Td isNumeric>{pending_ads}</Td>
+      <Td>
+        {is_muted ? (
+          <Button leftIcon={<FaVolumeMute />} colorScheme="red"></Button>
+        ) : (
+          <Button leftIcon={<FaVolumeUp />} colorScheme="green"></Button>
+        )}
+      </Td>
+    </Tr>
+  );
+};
+
+export default UserTableEntry;
